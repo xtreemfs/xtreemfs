cb2c1fadec13f671a659f052ebc289d27e6bffc6
Minor.Gordon <Minor.Gordon@f8528748-c2c0-11dd-ba5c-d3b7c2b3b534>
2009-03-12 16:17:42 +0000

    constants.idl: O_ constants
    git-svn-id: https://xtreemfs.googlecode.com/svn/trunk@224 f8528748-c2c0-11dd-ba5c-d3b7c2b3b534

--- servers/src/org/xtreemfs/interfaces/Constants.java
+++ servers/src/org/xtreemfs/interfaces/Constants.java
@@ -16,11 +16,11 @@ public interface Constants
     public static final int SERVICE_TYPE_VOLUME = 3;
     public static final int STRIPING_POLICY_DEFAULT = 0;
     public static final int STRIPING_POLICY_RAID0 = 1;
-    public static final int O_RDONLY = 0x0000;
-    public static final int O_WRONLY = 0x0001;
-    public static final int O_RDWR = 0x0002;
-    public static final int O_APPEND = 0x0008;
-    public static final int O_CREAT = 0x0100;
-    public static final int O_TRUNC = 0x0200;
-    public static final int O_EXCL = 0x0400;
+    public static final int SYSTEM_V_FCNTL_H_O_RDONLY = 0x0000;
+    public static final int SYSTEM_V_FCNTL_H_O_WRONLY = 0x0001;
+    public static final int SYSTEM_V_FCNTL_H_O_RDWR = 0x0002;
+    public static final int SYSTEM_V_FCNTL_H_O_APPEND = 0x0008;
+    public static final int SYSTEM_V_FCNTL_H_O_CREAT = 0x0100;
+    public static final int SYSTEM_V_FCNTL_H_O_TRUNC = 0x0200;
+    public static final int SYSTEM_V_FCNTL_H_O_EXCL = 0x0400;
 };
