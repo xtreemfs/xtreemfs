b47edad1c5174b7b32bc64502779bdf6779c7d6e
stenjan <stenjan@f8528748-c2c0-11dd-ba5c-d3b7c2b3b534>
2009-04-06 14:29:26 +0000

    * MRC bugfix: open flag 'O_EXCL' exhibits a proper behavior now
    git-svn-id: https://xtreemfs.googlecode.com/svn/trunk@353 f8528748-c2c0-11dd-ba5c-d3b7c2b3b534

--- servers/src/org/xtreemfs/mrc/operations/OpenOperation.java
+++ servers/src/org/xtreemfs/mrc/operations/OpenOperation.java
@@ -93,6 +93,11 @@ public class OpenOperation extends MRCOperation {
                 
                 res.checkIfFileDoesNotExist();
                 
+                // check if O_CREAT and O_EXCL are set; if so, send an exception
+                if ((rqArgs.getFlags() & FileAccessManager.O_CREAT) != 0
+                    && (rqArgs.getFlags() & FileAccessManager.O_EXCL) != 0)
+                    res.checkIfFileExistsAlready();
+                
                 file = res.getFile();
                 
                 // if the file refers to a symbolic link, resolve the link
@@ -133,16 +138,12 @@ public class OpenOperation extends MRCOperation {
                 // if the file does not exist, check whether the O_CREAT flag
                 // has been provided
                 if (exc.getErrno() == ErrNo.ENOENT && (rqArgs.getFlags() & FileAccessManager.O_CREAT) != 0) {
-
-                    //check for write permission in parent dir
+                    
+                    // check for write permission in parent dir
                     // check whether the parent directory grants write access
                     faMan.checkPermission(FileAccessManager.O_WRONLY, sMan, res.getParentDir(), res
-                            .getParentsParentId(), rq.getDetails().userId, rq.getDetails().superUser,
-                        rq.getDetails().groupIds);
-
-                    // prepare file creation in database
-                    if ((rqArgs.getFlags() & FileAccessManager.O_EXCL) != 0)
-                        res.checkIfFileExistsAlready();
+                            .getParentsParentId(), rq.getDetails().userId, rq.getDetails().superUser, rq
+                            .getDetails().groupIds);
                     
                     // get the next free file ID
                     long fileId = sMan.getNextFileId();
