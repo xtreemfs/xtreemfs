d9b00ed7e456210d1f262c0e6f637ea1db77e766
Minor.Gordon <Minor.Gordon@f8528748-c2c0-11dd-ba5c-d3b7c2b3b534>
2009-04-10 10:56:36 +0000

    client: small fix to appease gcc
    git-svn-id: https://xtreemfs.googlecode.com/svn/trunk@402 f8528748-c2c0-11dd-ba5c-d3b7c2b3b534

--- client/include/org/xtreemfs/client/volume.h
+++ client/include/org/xtreemfs/client/volume.h
@@ -28,6 +28,9 @@ namespace org
 
       class SharedFileCallbackInterface
       {
+      public:
+        virtual ~SharedFileCallbackInterface() { }
+
       private:
         friend class SharedFile;
 
