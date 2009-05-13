5231ac90ed524a979d753bc60f4247c7107a96ca
stenjan <stenjan@f8528748-c2c0-11dd-ba5c-d3b7c2b3b534>
2009-05-13 14:36:14 +0000

    MRC backend: fixed bug in BabuDB that caused incorrect file metadata lookups
    git-svn-id: https://xtreemfs.googlecode.com/svn/trunk@672 f8528748-c2c0-11dd-ba5c-d3b7c2b3b534

--- src/servers/src/org/xtreemfs/mrc/database/babudb/AtomicBabuDBUpdate.java
+++ src/servers/src/org/xtreemfs/mrc/database/babudb/AtomicBabuDBUpdate.java
@@ -23,6 +23,14 @@
  */
 package org.xtreemfs.mrc.database.babudb;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
 import org.xtreemfs.babudb.BabuDB;
 import org.xtreemfs.babudb.BabuDBException;
 import org.xtreemfs.babudb.BabuDBInsertGroup;
@@ -40,29 +48,41 @@ public class AtomicBabuDBUpdate implements AtomicDBUpdate {
     
     private Object                context;
     
-    public AtomicBabuDBUpdate(BabuDB database, String dbName, BabuDBRequestListener listener,
-        Object context) throws BabuDBException {
+//    private List<Object[]>        updates;
+//    
+//    private String                dbName;
+    
+    public AtomicBabuDBUpdate(BabuDB database, String dbName, BabuDBRequestListener listener, Object context)
+        throws BabuDBException {
         
         ig = database.createInsertGroup(dbName);
         
         this.database = database;
         this.listener = listener;
         this.context = context;
+        
+//        updates = new LinkedList<Object[]>();
+//        this.dbName = dbName;
     }
     
     @Override
     public void addUpdate(Object... update) {
         ig.addInsert((Integer) update[0], (byte[]) update[1], (byte[]) update[2]);
+//        updates.add(update);
     }
     
     @Override
     public void execute() throws DatabaseException {
         try {
+            
+            // checkDBConsistency();
+            
             if (listener != null) {
                 database.directInsert(ig);
                 listener.insertFinished(context);
             } else
                 database.directInsert(ig);
+            
         } catch (BabuDBException exc) {
             throw new DatabaseException(exc);
         }
@@ -71,5 +91,72 @@ public class AtomicBabuDBUpdate implements AtomicDBUpdate {
     public String toString() {
         return ig.toString();
     }
+    
+//    private void checkDBConsistency() {
+//        
+//        Map<String, byte[][]> prefixEntries = new HashMap<String, byte[][]>();
+//        
+//        for (Object[] update : updates) {
+//            if ((Integer) update[0] == BabuDBStorageManager.FILE_INDEX) {
+//                
+//                byte[] bytes = (byte[]) update[1];
+//                byte type = bytes[bytes.length - 1];
+//                
+//                byte[] prefix = new byte[bytes.length - 1];
+//                System.arraycopy(bytes, 0, prefix, 0, prefix.length);
+//                
+//                byte[][] entry = prefixEntries.get(new String(prefix));
+//                if (entry == null) {
+//                    
+//                    entry = new byte[3][];
+//                    prefixEntries.put(new String(prefix), entry);
+//                    
+//                    Iterator<Entry<byte[], byte[]>> entries = null;
+//                    try {
+//                        entries = database
+//                                .directPrefixLookup(dbName, BabuDBStorageManager.FILE_INDEX, prefix);
+//                    } catch (BabuDBException e) {
+//                        e.printStackTrace();
+//                        System.exit(1);
+//                    }
+//                    
+//                    while (entries.hasNext()) {
+//                        byte[] key = entries.next().getKey();
+//                        byte entryType = key[key.length - 1];
+//                        entry[entryType] = key;
+//                    }
+//                    
+//                }
+//                
+//                // do the update in memory
+//                entry[type] = update[2] == null ? null : (byte[]) update[1];
+//            }
+//        }
+//        
+//        // check the data structure
+//        for (byte[][] entry : prefixEntries.values()) {
+//            if ((entry[0] != null || entry[2] != null) && entry[1] == null) {
+//                System.err.println("CORRUPTED DATABASE!");
+//                System.err.println("updated entry:");
+//                for (int i = 0; i < entry.length; i++)
+//                    System.out.println(i + ": " + Arrays.toString(entry[i]));
+//                
+//                System.err.println("all updates:");
+//                for (Object[] update : updates) {
+//                    System.out.println(update[0] + ": " + Arrays.toString((byte[]) update[1]) + " = "
+//                        + Arrays.toString((byte[]) update[2]));
+//                }
+//                
+//                try {
+//                    throw new Exception();
+//                } catch (Exception exc) {
+//                    exc.printStackTrace();
+//                }
+//                
+//                System.exit(1);
+//            }
+//        }
+//        
+//    }
     
 }
