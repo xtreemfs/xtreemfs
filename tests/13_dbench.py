c37ba1a0f6c5fd7e762bb8569227ae6e02f4d137
Minor.Gordon <Minor.Gordon@f8528748-c2c0-11dd-ba5c-d3b7c2b3b534>
2009-05-14 15:38:57 +0000

    tests/13_dbench.py: skip non-direct volumes
    git-svn-id: https://xtreemfs.googlecode.com/svn/trunk@695 f8528748-c2c0-11dd-ba5c-d3b7c2b3b534

--- tests/13_dbench.py
+++ tests/13_dbench.py
@@ -7,22 +7,31 @@ DBENCH_CLIENT_TXT_GZ_FILE_PATH = os.path.join( MY_DIR_PATH, "dbench-client.txt.g
                                
 
 class dbenchTest(unittest.TestCase):
+    def __init__( self, direct_io=True, stdout=sys.stdout, stderr=sys.stderr, *args, **kwds ):
+        unittest.TestCase.__init__( self )
+        self.direct_io = direct_io
+        self.stdout = stdout
+        self.stderr = stderr
+
     def runTest( self ):
-        gzip_client_txt_gz_data = gzip.GzipFile( DBENCH_CLIENT_TXT_GZ_FILE_PATH, mode="rb" ).read()
-        assert len( gzip_client_txt_gz_data ) > 0
-        open( "dbench-client.txt", "wb" ).write( gzip_client_txt_gz_data )
-        assert os.stat( "dbench-client.txt" ).st_size > 0
+        if self.direct_io:
+           gzip_client_txt_gz_data = gzip.GzipFile( DBENCH_CLIENT_TXT_GZ_FILE_PATH, mode="rb" ).read()
+           assert len( gzip_client_txt_gz_data ) > 0
+           open( "dbench-client.txt", "wb" ).write( gzip_client_txt_gz_data )
+           assert os.stat( "dbench-client.txt" ).st_size > 0
         
-        args = "dbench -c dbench-client.txt -D . 5"
-        stdout = open( "dbench-stdout.txt", "a+" )
-        p = subprocess.Popen( args, shell=True, stdout=stdout, stderr=subprocess.STDOUT )
-        retcode = p.wait()
-        self.assertEqual( retcode, 0 )
+           args = "dbench -c dbench-client.txt -D . 5"
+           stdout = open( "dbench-stdout.txt", "a+" )
+           p = subprocess.Popen( args, shell=True, stdout=stdout, stderr=subprocess.STDOUT )
+           retcode = p.wait()
+           self.assertEqual( retcode, 0 )
+        else:
+            print >>self.stdout, self.__class__.__name__ + ": skipping nondirect volume", os.getcwd()
 
 
 def createTestSuite( *args, **kwds ): 
     if not sys.platform.startswith( "win" ):
-        return unittest.TestSuite( [dbenchTest()] )
+        return unittest.TestSuite( [dbenchTest( *args, **kwds )] )
                 
 
 if __name__ == "__main__":
