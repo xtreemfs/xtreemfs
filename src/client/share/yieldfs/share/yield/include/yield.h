1ae6a74a4fdcce91d19562d36add468c7766e15d
Minor.Gordon <Minor.Gordon@f8528748-c2c0-11dd-ba5c-d3b7c2b3b534>
2009-07-14 12:29:28 +0000

    client: updated against Yield
    git-svn-id: https://xtreemfs.googlecode.com/svn/trunk@923 f8528748-c2c0-11dd-ba5c-d3b7c2b3b534

--- src/client/share/yieldfs/share/yield/include/yield.h
+++ src/client/share/yieldfs/share/yield/include/yield.h
@@ -1,8 +1,8 @@
 // Copyright 2003-2008 Minor Gordon, with original implementations and ideas contributed by Felix Hupfeld.
 // This source comes from the Yield project. It is licensed under the GPLv2 (see COPYING for terms and conditions).
 
-#ifndef _YIELD_H
-#define _YIELD_H
+#ifndef _YIELD_H_
+#define _YIELD_H_
 
 #include "yield/concurrency.h"
 #include "yield/ipc.h"
