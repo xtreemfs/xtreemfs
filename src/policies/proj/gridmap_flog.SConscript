c8b47a136a6c58e4fe9135dd5016ae0d06b704cd
Minor.Gordon <Minor.Gordon@f8528748-c2c0-11dd-ba5c-d3b7c2b3b534>
2009-08-27 10:36:19 +0000

    policies: added gridmap_flog module
    git-svn-id: https://xtreemfs.googlecode.com/svn/trunk@1112 f8528748-c2c0-11dd-ba5c-d3b7c2b3b534

---
+++ src/policies/proj/gridmap_flog.SConscript
@@ -0,0 +1,4 @@
+Import( "build_env" )
+
+build_env.SharedLibrary( "../lib/gridmap_flog", (
+    r"../src/gridmap_flog.c" ) )
---
+++ src/policies/src/gridmap_flog.c
@@ -0,0 +1,13 @@
+#include "xtreemfs/policy.h"
+
+#include <string.h>
+
+
+DLLEXPORT int get_passwd_from_user_credentials( const char* user_id, const char* group_ids, int* uid, int* gid )
+{
+}
+
+DLLEXPORT int get_user_credentials_from_passwd( int uid, int gid, char* user_id, size_t* user_id_len, char* group_ids, size_t* group_ids_len )
+{
+}
+
