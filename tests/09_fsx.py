39037dea38ab875c3285dba9d27035f22dd3d5e4
bjoern.kolbeck <bjoern.kolbeck@f8528748-c2c0-11dd-ba5c-d3b7c2b3b534>
2009-12-09 15:38:08 +0000

    tests: added fsx
    git-svn-id: https://xtreemfs.googlecode.com/svn/trunk@1458 f8528748-c2c0-11dd-ba5c-d3b7c2b3b534

---
+++ tests/09_fsx.py
@@ -0,0 +1,40 @@
+import distutils.ccompiler
+import os.path
+import unittest, subprocess, sys, os
+
+MY_DIR_PATH = os.path.dirname( os.path.abspath( sys.modules[__name__].__file__ ) )
+
+class fsxTest(unittest.TestCase):
+    def __init__( self, direct_io=True, stdout=sys.stdout, stderr=sys.stderr, *args, **kwds ):
+        unittest.TestCase.__init__( self )
+        self.direct_io = direct_io
+        self.stdout = stdout
+        self.stderr = stderr
+        
+    def runTest( self ):
+        if os.path.exists(MY_DIR_PATH+"/utils/fsx.bin") == False:
+            compiler = distutils.ccompiler.new_compiler()
+            cwd = os.getcwd()
+            os.chdir(MY_DIR_PATH+"/utils")
+
+            compiler.compile(["ltp-fsx.c"])
+            compiler.link_executable(["ltp-fsx.o"],"fsx.bin")
+            os.chdir(cwd)
+
+        args = MY_DIR_PATH+"/utils/fsx.bin -R -W -N 100000 ./fsx.tmpfile" #  -s 100"
+        p = subprocess.Popen( args, shell=True, stdout=self.stdout, stderr=self.stderr )
+        retcode = p.wait()
+        self.assertEqual( retcode, 0 )
+        
+
+def createTestSuite( *args, **kwds ): 
+    if not sys.platform.startswith( "win" ):
+        return unittest.TestSuite( [fsxTest( *args, **kwds )] )
+        
+
+if __name__ == "__main__":
+    if not sys.platform.startswith( "win" ):
+        unittest.TextTestRunner( verbosity=2 ).run( createTestSuite() )
+    else:
+        print sys.modules[__name__].__file__.split( os.sep )[-1], "not supported on Windows"
+    
