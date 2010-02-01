8a36c4f98c2b6db90018e5fa2dc54ea9c8496847
bjoern.kolbeck <bjoern.kolbeck@f8528748-c2c0-11dd-ba5c-d3b7c2b3b534>
2010-02-01 09:24:02 +0000

    packaging: added xtreemfs group set-up to postinstall script
    git-svn-id: https://xtreemfs.googlecode.com/svn/trunk@1538 f8528748-c2c0-11dd-ba5c-d3b7c2b3b534

--- packaging/postinstall_setup.sh
+++ packaging/postinstall_setup.sh
@@ -3,12 +3,19 @@
 XTREEMFS_LOG_DIR=/var/log/xtreemfs
 XTREEMFS_HOME=/var/lib/xtreemfs
 XTREEMFS_USER=xtreemfs
+XTREEMFS_GROUP=xtreemfs
 
 exists=`grep -c $XTREEMFS_USER /etc/passwd`
+group_exists=`grep -c $XTREEMFS_GROUP /etc/group`
+if [ $group_exists -eq 0 ]
+then
+	groupadd $XTREEMFS_GROUP
+	echo "created group XTREEMFS_GROUP"
+fu
 if [ $exists -eq 0 ]
 then
         mkdir $XTREEMFS_HOME
-        useradd -r --home $XTREEMFS_HOME $XTREEMFS_USER
+        useradd -r --home $XTREEMFS_HOME -g $XTREEMFS_GROUP $XTREEMFS_USER
         chown $XTREEMFS_USER $XTREEMFS_HOME
         echo "created user $XTREEMFS_USER and data directory $XTREEMFS_HOME"
 else
