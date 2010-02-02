07e683db30045f993386a871609a28d0dd603374
bjoern.kolbeck <bjoern.kolbeck@f8528748-c2c0-11dd-ba5c-d3b7c2b3b534>
2010-02-02 08:47:25 +0000

    release 1.2.1: marged fixes for manpages from trunk, changed the version string
    git-svn-id: https://xtreemfs.googlecode.com/svn/branches/XtreemFS-1.2.1@1542 f8528748-c2c0-11dd-ba5c-d3b7c2b3b534

--- src/servers/src/org/xtreemfs/common/VersionManagement.java
+++ src/servers/src/org/xtreemfs/common/VersionManagement.java
@@ -38,7 +38,7 @@ package org.xtreemfs.common;
  */
 public class VersionManagement {
 
-    public static final String  RELEASE_VERSION = "1.2.1 (TRUNK)";
+    public static final String  RELEASE_VERSION = "1.2.1 (Luscious Lebkuchen, update 1)";
 
     private static final long   mrcDataVersion            = 10;
 
