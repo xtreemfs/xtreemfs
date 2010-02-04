dd47efe7453250161f53c02d70b908df5aacdda7
Minor.Gordon <Minor.Gordon@f8528748-c2c0-11dd-ba5c-d3b7c2b3b534>
2010-02-04 14:28:36 +0000

    client: nettest: send_buffer, recv_buffer operations, filling out the client
    git-svn-id: https://xtreemfs.googlecode.com/svn/trunk@1562 f8528748-c2c0-11dd-ba5c-d3b7c2b3b534

--- src/nettest.xtreemfs/nettest.xtreemfs.cpp
+++ src/nettest.xtreemfs/nettest.xtreemfs.cpp
@@ -7,6 +7,11 @@
 #include <iostream>
 
 
+// Constants
+#define RATE_DEFAULT 1
+#define NUM_CALLS_DEFAULT 1
+
+
 namespace nettest_xtreemfs
 {
   class Main : public xtreemfs::Main
@@ -19,17 +24,91 @@ namespace nettest_xtreemfs
           "test the network connection to an XtreemFS server",
           "[oncrpc://]<host>:port"
         )
-    { }
+    {
+      addOption
+      (
+        NETTEST_XTREEMFS_OPTION_NUM_CALLS, 
+        "-n",
+        "--num-calls",
+        "number of RPCs to send"        
+      );
+      num_calls = NUM_CALLS_DEFAULT;
+
+      addOption
+      (
+        NETTEST_XTREEMFS_OPTION_RATE, 
+        "-r",
+        "--rate",
+        "rate to send RPCs in RPCs/s"
+      );
+      rate = RATE_DEFAULT;
+
+      addOption
+      (
+        NETTEST_XTREEMFS_OPTION_RECV_BUFFER, 
+        "--recv-buffer",
+        NULL,
+        "receive buffers with the given size (power of 2, up to 2MB)"
+      );
+
+      addOption
+      (
+        NETTEST_XTREEMFS_OPTION_SEND_BUFFER, 
+        "--send-buffer",
+        NULL,
+        "send buffers with the given size"
+      );
+    }
 
   private:
+    enum
+    {
+      NETTEST_XTREEMFS_OPTION_NUM_CALLS = 20,
+      NETTEST_XTREEMFS_OPTION_RATE = 21,
+      NETTEST_XTREEMFS_OPTION_RECV_BUFFER = 22,
+      NETTEST_XTREEMFS_OPTION_SEND_BUFFER = 23,
+    };
+
+    uint32_t num_calls;
+    uint32_t rate;
+    yidl::runtime::auto_Buffer recv_buffer;
+    yidl::runtime::auto_Buffer send_buffer;
     YIELD::ipc::auto_URI uri;
+
 
     // YIELD::Main
     int _main( int, char** )
     {
-      auto_NettestProxy nettest_proxy( NettestProxy::create( *uri ) );
-    
-      nettest_proxy->nop();
+      auto_NettestProxy 
+        nettest_proxy
+        ( 
+          NettestProxy::create
+          ( 
+            *uri,
+            NettestProxy::CONCURRENCY_LEVEL_DEFAULT,
+            0,
+            get_log(),
+            get_operation_timeout(),
+            NettestProxy::RECONNECT_TRIES_MAX_DEFAULT,
+            get_proxy_ssl_context()
+          ) 
+        );         
+
+      uint64_t sleep_ns 
+        = static_cast<uint64_t>( ( 1.0 / static_cast<double>( rate ) ) 
+          * static_cast<double>( NS_IN_S ) );
+
+      for ( uint32_t call_i = 0; call_i < num_calls; call_i++ )
+      {
+        if ( recv_buffer != NULL )
+          nettest_proxy->recv_buffer( recv_buffer->capacity(), recv_buffer );
+        else if ( send_buffer != NULL )
+          nettest_proxy->send_buffer( send_buffer );
+        else
+          nettest_proxy->nop();
+
+        YIELD::platform::Thread::nanosleep( sleep_ns );
+      }
 
       return 0;
     }
@@ -44,6 +123,54 @@ namespace nettest_xtreemfs
       }
       else
         throw YIELD::platform::Exception( "must specify a URI" );
+    }
+
+    void parseOption( int id, char* arg )
+    {
+      switch ( id )
+      {
+        case NETTEST_XTREEMFS_OPTION_NUM_CALLS:
+        {
+          num_calls = atoi( arg );
+          if ( num_calls == 0 )
+            num_calls = NUM_CALLS_DEFAULT;
+        }
+        break;
+
+        case NETTEST_XTREEMFS_OPTION_RATE:
+        {
+          rate = atoi( arg );
+          if ( rate == 0 )
+            rate = RATE_DEFAULT;
+        }
+        break;
+
+        case NETTEST_XTREEMFS_OPTION_RECV_BUFFER:
+        {
+          // arg should be a power of 2 up to 2MB
+          // Then allocate recv_buffer
+        }
+        break;
+
+        case NETTEST_XTREEMFS_OPTION_SEND_BUFFER:
+        {
+          uint32_t capacity = atoi( arg );
+          if ( capacity > 0 )
+          {
+            send_buffer 
+              = new yidl::runtime::HeapBuffer( capacity );
+            for ( uint32_t byte_i = 0; byte_i < capacity; byte_i++ )
+              send_buffer->put( "m", 1 );
+          }
+        }
+        break;
+
+        default:
+        {
+          xtreemfs::Main::parseOption( id, arg );
+        }
+        break;
+      }
     }
   };
 };
--- src/nettest.xtreemfs/nettest_interface.h
+++ src/nettest.xtreemfs/nettest_interface.h
@@ -68,6 +68,12 @@ namespace org
         virtual void nop() { nop( static_cast<uint64_t>( -1 ) ); }
         virtual void nop( uint64_t response_timeout_ns ) { ::yidl::runtime::auto_Object<nopRequest> __request( new nopRequest() ); ::YIELD::concurrency::auto_ResponseQueue<nopResponse> __response_queue( new ::YIELD::concurrency::ResponseQueue<nopResponse> ); __request->set_response_target( __response_queue->incRef() ); send( __request->incRef() ); ::yidl::runtime::auto_Object<nopResponse> __response = response_timeout_ns == static_cast<uint64_t>( -1 ) ? __response_queue->dequeue() : __response_queue->timed_dequeue( response_timeout_ns ); }
   
+        virtual void send_buffer( ::yidl::runtime::auto_Buffer data ) { send_buffer( data, static_cast<uint64_t>( -1 ) ); }
+        virtual void send_buffer( ::yidl::runtime::auto_Buffer data, uint64_t response_timeout_ns ) { ::yidl::runtime::auto_Object<send_bufferRequest> __request( new send_bufferRequest( data ) ); ::YIELD::concurrency::auto_ResponseQueue<send_bufferResponse> __response_queue( new ::YIELD::concurrency::ResponseQueue<send_bufferResponse> ); __request->set_response_target( __response_queue->incRef() ); send( __request->incRef() ); ::yidl::runtime::auto_Object<send_bufferResponse> __response = response_timeout_ns == static_cast<uint64_t>( -1 ) ? __response_queue->dequeue() : __response_queue->timed_dequeue( response_timeout_ns ); }
+  
+        virtual void recv_buffer( uint32_t size, ::yidl::runtime::auto_Buffer data ) { recv_buffer( size, data, static_cast<uint64_t>( -1 ) ); }
+        virtual void recv_buffer( uint32_t size, ::yidl::runtime::auto_Buffer data, uint64_t response_timeout_ns ) { ::yidl::runtime::auto_Object<recv_bufferRequest> __request( new recv_bufferRequest( size ) ); ::YIELD::concurrency::auto_ResponseQueue<recv_bufferResponse> __response_queue( new ::YIELD::concurrency::ResponseQueue<recv_bufferResponse> ); __request->set_response_target( __response_queue->incRef() ); send( __request->incRef() ); ::yidl::runtime::auto_Object<recv_bufferResponse> __response = response_timeout_ns == static_cast<uint64_t>( -1 ) ? __response_queue->dequeue() : __response_queue->timed_dequeue( response_timeout_ns ); data = __response->get_data().release(); }
+  
   
         // Request/response pair definitions for the operations in NettestInterface
   
@@ -99,6 +105,92 @@ namespace org
   
         };
   
+        class send_bufferResponse : public ORG_XTREEMFS_INTERFACES_NETTESTINTERFACE_RESPONSE_PARENT_CLASS
+        {
+        public:
+          send_bufferResponse() { }
+          virtual ~send_bufferResponse() { }
+  
+          bool operator==( const send_bufferResponse& ) const { return true; }
+  
+          // yidl::runtime::Object
+          YIDL_RUNTIME_OBJECT_PROTOTYPES( send_bufferResponse, 2010012515 );
+  
+        };
+  
+        class send_bufferRequest : public ORG_XTREEMFS_INTERFACES_NETTESTINTERFACE_REQUEST_PARENT_CLASS
+        {
+        public:
+          send_bufferRequest() { }
+          send_bufferRequest( ::yidl::runtime::auto_Buffer data ) : data( data ) { }
+          virtual ~send_bufferRequest() { }
+  
+          void set_data( ::yidl::runtime::auto_Buffer data ) { this->data = data; }
+          ::yidl::runtime::auto_Buffer get_data() const { return data; }
+  
+          bool operator==( const send_bufferRequest& other ) const { return *data == *other.data; }
+  
+          // yidl::runtime::Object
+          YIDL_RUNTIME_OBJECT_PROTOTYPES( send_bufferRequest, 2010012515 );
+  
+          // yidl::Struct
+          void marshal( ::yidl::runtime::Marshaller& marshaller ) const { marshaller.writeBuffer( "data", 0, data ); }
+          void unmarshal( ::yidl::runtime::Unmarshaller& unmarshaller ) { unmarshaller.readBuffer( "data", 0, data ); }  // YIELD::concurrency::Request
+          virtual ::YIELD::concurrency::auto_Response createResponse() { return new send_bufferResponse; }
+  
+  
+        protected:
+          ::yidl::runtime::auto_Buffer data;
+        };
+  
+        class recv_bufferResponse : public ORG_XTREEMFS_INTERFACES_NETTESTINTERFACE_RESPONSE_PARENT_CLASS
+        {
+        public:
+          recv_bufferResponse() { }
+          recv_bufferResponse( ::yidl::runtime::auto_Buffer data ) : data( data ) { }
+          virtual ~recv_bufferResponse() { }
+  
+          void set_data( ::yidl::runtime::auto_Buffer data ) { this->data = data; }
+          ::yidl::runtime::auto_Buffer get_data() const { return data; }
+  
+          bool operator==( const recv_bufferResponse& other ) const { return *data == *other.data; }
+  
+          // yidl::runtime::Object
+          YIDL_RUNTIME_OBJECT_PROTOTYPES( recv_bufferResponse, 2010012516 );
+  
+          // yidl::Struct
+          void marshal( ::yidl::runtime::Marshaller& marshaller ) const { marshaller.writeBuffer( "data", 0, data ); }
+          void unmarshal( ::yidl::runtime::Unmarshaller& unmarshaller ) { unmarshaller.readBuffer( "data", 0, data ); }
+  
+        protected:
+          ::yidl::runtime::auto_Buffer data;
+        };
+  
+        class recv_bufferRequest : public ORG_XTREEMFS_INTERFACES_NETTESTINTERFACE_REQUEST_PARENT_CLASS
+        {
+        public:
+          recv_bufferRequest() : size( 0 ) { }
+          recv_bufferRequest( uint32_t size ) : size( size ) { }
+          virtual ~recv_bufferRequest() { }
+  
+          void set_size( uint32_t size ) { this->size = size; }
+          uint32_t get_size() const { return size; }
+  
+          bool operator==( const recv_bufferRequest& other ) const { return size == other.size; }
+  
+          // yidl::runtime::Object
+          YIDL_RUNTIME_OBJECT_PROTOTYPES( recv_bufferRequest, 2010012516 );
+  
+          // yidl::Struct
+          void marshal( ::yidl::runtime::Marshaller& marshaller ) const { marshaller.writeUint32( "size", 0, size ); }
+          void unmarshal( ::yidl::runtime::Unmarshaller& unmarshaller ) { size = unmarshaller.readUint32( "size", 0 ); }  // YIELD::concurrency::Request
+          virtual ::YIELD::concurrency::auto_Response createResponse() { return new recv_bufferResponse; }
+  
+  
+        protected:
+          uint32_t size;
+        };
+  
   
   
         // yidl::runtime::Object
@@ -113,6 +205,8 @@ namespace org
             switch ( ev.get_type_id() )
             {
               case 2010012514UL: handlenopRequest( static_cast<nopRequest&>( ev ) ); return;
+              case 2010012515UL: handlesend_bufferRequest( static_cast<send_bufferRequest&>( ev ) ); return;
+              case 2010012516UL: handlerecv_bufferRequest( static_cast<recv_bufferRequest&>( ev ) ); return;
               default: handleUnknownEvent( ev ); return;
             }
           }
@@ -139,6 +233,8 @@ namespace org
             switch ( request.get_type_id() )
             {
               case 2010012514: return static_cast<nopRequest*>( &request );
+              case 2010012515: return static_cast<send_bufferRequest*>( &request );
+              case 2010012516: return static_cast<recv_bufferRequest*>( &request );
               default: return NULL;
             }
           }
@@ -148,6 +244,8 @@ namespace org
             switch ( response.get_type_id() )
             {
               case 2010012514: return static_cast<nopResponse*>( &response );
+              case 2010012515: return static_cast<send_bufferResponse*>( &response );
+              case 2010012516: return static_cast<recv_bufferResponse*>( &response );
               default: return NULL;
             }
           }
@@ -157,6 +255,8 @@ namespace org
             switch ( tag )
             {
               case 2010012514: return new nopRequest;
+              case 2010012515: return new send_bufferRequest;
+              case 2010012516: return new recv_bufferRequest;
               default: return NULL;
             }
           }
@@ -166,6 +266,8 @@ namespace org
             switch ( tag )
             {
               case 2010012514: return new nopResponse;
+              case 2010012515: return new send_bufferResponse;
+              case 2010012516: return new recv_bufferResponse;
               default: return NULL;
             }
           }
@@ -175,16 +277,24 @@ namespace org
   
       protected:
         virtual void handlenopRequest( nopRequest& req ) { ::yidl::runtime::auto_Object<nopResponse> resp( new nopResponse ); _nop(); req.respond( *resp.release() ); ::yidl::runtime::Object::decRef( req ); }
+        virtual void handlesend_bufferRequest( send_bufferRequest& req ) { ::yidl::runtime::auto_Object<send_bufferResponse> resp( new send_bufferResponse ); _send_buffer( req.get_data() ); req.respond( *resp.release() ); ::yidl::runtime::Object::decRef( req ); }
+        virtual void handlerecv_bufferRequest( recv_bufferRequest& req ) { ::yidl::runtime::auto_Object<recv_bufferResponse> resp( new recv_bufferResponse ); ::yidl::runtime::auto_Buffer data; _recv_buffer( req.get_size(), data ); resp->set_data( data ); req.respond( *resp.release() ); ::yidl::runtime::Object::decRef( req ); }
   
       virtual void _nop() { }
+        virtual void _send_buffer( ::yidl::runtime::auto_Buffer  ) { }
+        virtual void _recv_buffer( uint32_t, ::yidl::runtime::auto_Buffer  ) { }
       };
   
       // Use this macro in an implementation class to get all of the prototypes for the operations in NettestInterface
       #define ORG_XTREEMFS_INTERFACES_NETTESTINTERFACE_PROTOTYPES \
-      virtual void _nop();
+      virtual void _nop();\
+      virtual void _send_buffer( ::yidl::runtime::auto_Buffer data );\
+      virtual void _recv_buffer( uint32_t size, ::yidl::runtime::auto_Buffer data );
   
       #define ORG_XTREEMFS_INTERFACES_NETTESTINTERFACE_HANDLEEVENT_PROTOTYPES \
-      virtual void handlenopRequest( nopRequest& req );
+      virtual void handlenopRequest( nopRequest& req );\
+      virtual void handlesend_bufferRequest( send_bufferRequest& req );\
+      virtual void handlerecv_bufferRequest( recv_bufferRequest& req );
   
     };
   
