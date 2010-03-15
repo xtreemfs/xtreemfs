4707431b956b79bc09a89ac3cb8658d8e1f21f26
stenjan <stenjan@f8528748-c2c0-11dd-ba5c-d3b7c2b3b534>
2010-03-15 12:22:00 +0000

    MRC: made read-only replication work again
    git-svn-id: https://xtreemfs.googlecode.com/svn/trunk@1691 f8528748-c2c0-11dd-ba5c-d3b7c2b3b534

--- src/servers/src/org/xtreemfs/mrc/operations/OpenOperation.java
+++ src/servers/src/org/xtreemfs/mrc/operations/OpenOperation.java
@@ -221,7 +221,8 @@ public class OpenOperation extends MRCOperation {
             assert (xLocList != null) : "The requested file has no xLocList!";
             
             xLocSet = Converter.xLocListToXLocSet(xLocList);
-            xLocSet.setReplica_update_policy(xLocSet.getReplica_update_policy());
+            xLocSet.setReplica_update_policy(file.isReadOnly() ? Constants.REPL_UPDATE_PC_RONLY
+                : xLocList.getReplUpdatePolicy());
             if (file.isReadOnly())
                 xLocSet.setRead_only_file_size(file.getSize());
         }
