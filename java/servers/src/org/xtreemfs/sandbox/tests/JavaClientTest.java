68d14e5b5c00faf55f7acf05268693e5fb1117a7
stenjan <stenjan@f8528748-c2c0-11dd-ba5c-d3b7c2b3b534>
2011-12-08 14:11:25 +0000

    added Java client test to reproduce issue 238
    git-svn-id: https://xtreemfs.googlecode.com/svn/trunk@2838 f8528748-c2c0-11dd-ba5c-d3b7c2b3b534

---
+++ java/servers/src/org/xtreemfs/sandbox/tests/JavaClientTest.java
@@ -0,0 +1,94 @@
+package org.xtreemfs.sandbox.tests;
+
+import java.net.InetSocketAddress;
+
+import org.xtreemfs.common.clients.Client;
+import org.xtreemfs.common.clients.File;
+import org.xtreemfs.common.clients.RandomAccessFile;
+import org.xtreemfs.common.clients.Volume;
+import org.xtreemfs.foundation.logging.Logging;
+import org.xtreemfs.foundation.pbrpc.generatedinterfaces.RPC.UserCredentials;
+
+public class JavaClientTest {
+    
+    private static final byte[] buf        = new byte[131072];
+    
+    private static final int    numThreads = 40;
+    
+    private static final int    numFiles   = 3000;
+    
+    private static final int    numAppends = 3;
+    
+    private Client              c;
+    
+    private Volume              v;
+    
+    public void init() throws Exception {
+        
+        UserCredentials uc = UserCredentials.newBuilder().setUsername("stender").addGroups("users").build();
+        
+        c = new Client(new InetSocketAddress[] { new InetSocketAddress(32638) }, 5000, 60000, null);
+        c.start();
+        
+        v = c.getVolume("test", uc);
+    }
+    
+    public void run() throws Exception {
+        
+        Thread[] threads = new Thread[numThreads];
+        for (int i = 0; i < threads.length; i++)
+            threads[i] = getWriter(i);
+        
+        for (Thread th : threads)
+            th.start();
+        
+        for (Thread th : threads)
+            th.join();
+        
+    }
+    
+    public void stop() {
+        c.stop();
+    }
+    
+    private Thread getWriter(final int num) {
+        
+        return new Thread() {
+            
+            public void run() {
+                
+                try {
+                    
+                    File dir = v.getFile(num + "");
+                    if (!dir.exists())
+                        dir.mkdir(0777);
+                    
+                    for (int i = 0; i < numFiles; i++) {
+                        File file = v.getFile(num + "/" + i + ".txt");
+                        
+                        RandomAccessFile raf = file.open("rw", 0777);
+                        for (int j = 0; j < numAppends; j++)
+                            raf.write(buf, 0, buf.length);
+                        raf.close();
+                    }
+                    
+                } catch (Exception exc) {
+                    exc.printStackTrace();
+                }
+                
+            }
+            
+        };
+    }
+    
+    public static void main(String[] args) throws Exception {
+        
+        Logging.start(Logging.LEVEL_WARN);
+        
+        JavaClientTest t = new JavaClientTest();
+        t.init();
+        t.run();
+        t.stop();
+    }
+    
+}
