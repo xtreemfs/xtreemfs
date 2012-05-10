a91d443739dd30ec71274404f7f98b1f5f74ba33
michael.berlin.xtreemfs@googlemail.com <michael.berlin.xtreemfs@googlemail.com@f8528748-c2c0-11dd-ba5c-d3b7c2b3b534>
2012-05-10 07:54:16 +0000

    packaging: Uninstall old XtreemFS version before installing new version.
    git-svn-id: https://xtreemfs.googlecode.com/svn/trunk@3041 f8528748-c2c0-11dd-ba5c-d3b7c2b3b534

--- packaging/macosx/pre_install.sh
+++ packaging/macosx/pre_install.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+XTREEMFS_UNINSTALL_SCRIPT="/usr/local/bin/uninstall_xtreemfs.sh"
+
+# Uninstall previous XtreemFS version.
+if [ -x "$XTREEMFS_UNINSTALL_SCRIPT" ]
+then
+  "$XTREEMFS_UNINSTALL_SCRIPT" noprompt
+  rm "$XTREEMFS_UNINSTALL_SCRIPT"
+fi
+
 # Create directories which do not exist yet.
 xtreemfs_dirs="/usr/local/share/xtreemfs /usr/local/bin"
 for dir in $xtreemfs_dirs
@@ -15,3 +24,5 @@ done
 # Create symlink "mount_xtreemfs" for Mac.
 ln -s /usr/local/bin/mount.xtreemfs /sbin/mount_xtreemfs
 
+# Exit succesfully.
+exit 0
