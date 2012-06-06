1f9d0ae035cc844476d8cd27e9f7b51e3ff63cb5
lukas.xtreemfs <lukas.xtreemfs@f8528748-c2c0-11dd-ba5c-d3b7c2b3b534>
2012-06-06 09:56:10 +0000

    trunk/servers: merged branch lukas-CleanUpDemoVolume_libxtreemfs into trunk
    git-svn-id: https://xtreemfs.googlecode.com/svn/trunk@3123 f8528748-c2c0-11dd-ba5c-d3b7c2b3b534

--- java/servers/src/org/xtreemfs/sandbox/CleanupDemoVolume.java
+++ java/servers/src/org/xtreemfs/sandbox/CleanupDemoVolume.java
@@ -1,6 +1,5 @@
 /*
- * Copyright (c) 2011 by Michael Berlin,
- *               Zuse Institute Berlin
+ * Copyright (c) 2012 by Lukas Kairies, Zuse Institute Berlin
  *
  * Licensed under the BSD License, see LICENSE file for details.
  *
@@ -10,90 +9,80 @@ package org.xtreemfs.sandbox;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.net.InetSocketAddress;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.xtreemfs.common.clients.Client;
-import org.xtreemfs.common.clients.File;
-import org.xtreemfs.common.clients.RandomAccessFile;
-import org.xtreemfs.common.clients.Volume;
+import org.xtreemfs.common.libxtreemfs.Client;
+import org.xtreemfs.common.libxtreemfs.FileHandle;
+import org.xtreemfs.common.libxtreemfs.Options;
+import org.xtreemfs.common.libxtreemfs.Volume;
 import org.xtreemfs.foundation.SSLOptions;
 import org.xtreemfs.foundation.logging.Logging;
 import org.xtreemfs.foundation.logging.Logging.Category;
 import org.xtreemfs.foundation.pbrpc.generatedinterfaces.RPC.UserCredentials;
 import org.xtreemfs.pbrpc.generatedinterfaces.GlobalTypes.SYSTEM_V_FCNTL;
 import org.xtreemfs.pbrpc.generatedinterfaces.MRC.DirectoryEntry;
+import org.xtreemfs.pbrpc.generatedinterfaces.MRC.Setattrs;
+import org.xtreemfs.pbrpc.generatedinterfaces.MRC.Stat;
 
-/**
- * 
- * Aug 22, 2011
- * 
- * @author mberlin
- */
 public class CleanupDemoVolume {
     private static Client          client;
     private static Volume          volume;
-    private static UserCredentials user_credentials;
-    private static List<String> example_files;
+    private static UserCredentials userCredentials;
+    private static List<String>    exampleFiles;
 
-    public static boolean DeleteDirectoryRecursively(String directory_path) {
-        boolean errors_occurred = false;
+    public static boolean DeleteDirectoryRecursively(String directoryPath) {
+        boolean errorsOccurred = false;
 
         try {
-            DirectoryEntry[] dentries = volume.listEntries(directory_path, user_credentials);
-
+            List<DirectoryEntry> dentries = volume.readDir(userCredentials, directoryPath, 0, 0, false)
+                    .getEntriesList();
             for (DirectoryEntry dentry : dentries) {
-                String full_path = (directory_path.equals("/") ? "" : directory_path) + "/" + dentry.getName();
+                String fullPath = (directoryPath.equals("/") ? "" : directoryPath) + "/" + dentry.getName();
                 if (dentry.getName().equals(".") || dentry.getName().equals("..")) {
                     continue;
                 }
-                boolean skip_entry = false;
-                for (String file : example_files) {
-                    if (full_path.equals("/" + new java.io.File(file).getName())) {
-                        skip_entry = true;
+                boolean skipEntry = false;
+                for (String file : exampleFiles) {
+                    if (fullPath.equals("/" + new java.io.File(file).getName())) {
+                        skipEntry = true;
                         break;
                     }
                 }
-                if (skip_entry) {
+                if (skipEntry) {
                     continue;
                 }
 
                 if ((dentry.getStbuf().getMode() & SYSTEM_V_FCNTL.SYSTEM_V_FCNTL_H_S_IFREG.getNumber()) != 0) {
                     // File.
                     try {
-                        File file = volume.getFile(directory_path + "/" + dentry.getName(), user_credentials);
-                        file.delete();
-                    } catch (FileNotFoundException e) {
+                        volume.unlink(userCredentials, fullPath);
+                    } catch (IOException e) {
                         // No error for us.
                     }
                 } else if ((dentry.getStbuf().getMode() & SYSTEM_V_FCNTL.SYSTEM_V_FCNTL_H_S_IFDIR.getNumber()) != 0) {
                     // Directory.
-                    DeleteDirectoryRecursively(directory_path + "/" + dentry.getName());
+                    DeleteDirectoryRecursively(fullPath);
                 }
             }
         } catch (FileNotFoundException e) {
             // Not counted as error.
         } catch (IOException e) {
             e.printStackTrace();
-            errors_occurred = true;
+            errorsOccurred = true;
         }
 
         // Delete directory itself.
-        if (!directory_path.equals("/")) {
+        if (!directoryPath.equals("/")) {
             try {
-                File file = volume.getFile(directory_path, user_credentials);
-                // No special rmdir available.
-                file.delete();
-            } catch (FileNotFoundException e) {
-                // Not counted as error.
+                volume.removeDirectory(userCredentials, directoryPath);
             } catch (IOException e) {
                 e.printStackTrace();
-                errors_occurred = true;
+                errorsOccurred = true;
             }
         }
 
-        return errors_occurred;
+        return errorsOccurred;
     }
 
     public static void main(String[] args) {
@@ -105,60 +94,63 @@ public class CleanupDemoVolume {
 
         Logging.start(Logging.LEVEL_ERROR, Category.tool);
 
-        user_credentials = UserCredentials.newBuilder().setUsername("root").addGroups("root").build();
+        userCredentials = UserCredentials.newBuilder().setUsername("root").addGroups("root").build();
 
-        InetSocketAddress dirAddresses[] = new InetSocketAddress[] { new InetSocketAddress(
-                "demo.xtreemfs.org", 32638) };
+        String dirAddress = "demo.xtreemfs.org:32638";
+
         SSLOptions sslOptions = null;
 
+        Options options = new Options();
+
         try {
-            client = new Client(dirAddresses, 15000, 60000, sslOptions);
+            client = Client.createClient(dirAddress, userCredentials, sslOptions, options);
             client.start();
-            volume = client.getVolume("demo", null);
+            volume = client.openVolume("demo", sslOptions, options);
         } catch (Exception e) {
             System.out.println("The client could not be started/the volume opened.");
             e.printStackTrace();
             System.exit(2);
         }
-        
+
         // Get file names of example files.
-        String example_files_dir = "/var/adm/xtreemfs/default_demo_files/";
-        example_files = new ArrayList<String>();
-        java.io.File dir = new java.io.File(example_files_dir);
+        String exampleFilesDir = "/var/adm/xtreemfs/default_demo_files/";
+        java.io.File dir = new java.io.File(exampleFilesDir);
         String[] files = dir.list();
+        exampleFiles = new ArrayList<String>(files.length);
         if (files != null) {
             for (String file : files) {
-                example_files.add(example_files_dir + file);
+                exampleFiles.add(exampleFilesDir + file);
             }
         }
 
         // Clean everything.
-        boolean errors_occurred = DeleteDirectoryRecursively("/");
+        boolean errorsOccurred = DeleteDirectoryRecursively("/");
 
         // Copy example files.
-        for (String filename : example_files) {
-            java.io.File source_file = new java.io.File(filename);
-
+        for (String filename : exampleFiles) {
+            java.io.File sourceFile = new java.io.File(filename);
             FileInputStream source = null;
-            RandomAccessFile target = null;
+            FileHandle target = null;
             try {
-                source = new FileInputStream(source_file);
-                File file = volume.getFile("/" + source_file.getName(), user_credentials);
-                target = file.open("rw", 292);
+                source = new FileInputStream(sourceFile);
+                target = volume.openFile(userCredentials, "/" + sourceFile.getName(),
+                        SYSTEM_V_FCNTL.SYSTEM_V_FCNTL_H_O_RDWR.getNumber()
+                                | SYSTEM_V_FCNTL.SYSTEM_V_FCNTL_H_O_CREAT.getNumber()
+                                | SYSTEM_V_FCNTL.SYSTEM_V_FCNTL_H_O_TRUNC.getNumber());
                 byte[] buffer = new byte[128 * 1024];
                 int bytesRead;
 
                 while ((bytesRead = source.read(buffer)) != -1) {
-                    target.write(buffer, 0, bytesRead);
+                    target.write(userCredentials, buffer, bytesRead, 0);
                 }
             } catch (FileNotFoundException e) {
                 System.out.println("File " + filename + " does not exist.");
                 e.printStackTrace();
-                errors_occurred = true;
+                errorsOccurred = true;
             } catch (IOException e) {
                 System.out.println("An IOError occurred when copying the file " + filename + ".");
                 e.printStackTrace();
-                errors_occurred = true;
+                errorsOccurred = true;
             } finally {
                 if (source != null) {
                     try {
@@ -166,42 +158,43 @@ public class CleanupDemoVolume {
                     } catch (IOException e) {
                         System.out.println("The source file " + filename + " could not be closed.");
                         e.printStackTrace();
-                        errors_occurred = true;
+                        errorsOccurred = true;
                     }
                 }
                 if (target != null) {
                     try {
                         target.close();
                     } catch (IOException e) {
-                        System.out.println("The target file /" + source_file.getName()
+                        System.out.println("The target file /" + sourceFile.getName()
                                 + " could not be closed.");
                         e.printStackTrace();
-                        errors_occurred = true;
+                        errorsOccurred = true;
                     }
                 }
             }
         }
-        
         // Truncate example files to actual size and chmod them.
-        for (String file : example_files) {
-            String file_name = new java.io.File(file).getName();
-            long file_size = new java.io.File(file).length();
-            File xtreemfs_file = volume.getFile("/" + file_name, user_credentials);
+        for (String file : exampleFiles) {
+            String fileName = new java.io.File(file).getName();
+            long fileSize = new java.io.File(file).length();
+            FileHandle xtreemfsFile = null;
             try {
-                xtreemfs_file.chmod(292);
-                RandomAccessFile xtreemfs_file_raf = xtreemfs_file.open("rw", 292);
-                xtreemfs_file_raf.setLength(file_size);
+                xtreemfsFile = volume.openFile(userCredentials, "/" + fileName,
+                        SYSTEM_V_FCNTL.SYSTEM_V_FCNTL_H_O_RDWR.getNumber());
+                Stat stbuf = xtreemfsFile.getAttr(userCredentials).toBuilder().setMode(0666).build();
+                volume.setAttr(userCredentials, "/" + fileName, stbuf, Setattrs.SETATTR_MODE.getNumber());
+                xtreemfsFile.truncate(userCredentials, fileSize);
+                xtreemfsFile.close();
             } catch (IOException e) {
-                System.out.println("Failed to truncate /" + file_name
-                        + " to the length " + file_size + ".");
+                System.out.println("Failed to truncate /" + fileName + " to the length " + fileSize + ".");
                 e.printStackTrace();
-                errors_occurred = true;
+                errorsOccurred = true;
             }
         }
 
-        client.stop();
+        client.shutdown();
 
-        if (errors_occurred) {
+        if (errorsOccurred) {
             System.out.println("Not all items could be deleted / files copied.");
             System.exit(3);
         }
