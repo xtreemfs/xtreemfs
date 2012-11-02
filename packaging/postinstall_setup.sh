9f7fdd0222f30eb6a41fc980939ec8924820a201
michael.berlin.xtreemfs@gmail.com <michael.berlin.xtreemfs@gmail.com@f8528748-c2c0-11dd-ba5c-d3b7c2b3b534>
2012-11-02 17:59:42 +0000

    packaging: Do not output actions in postinstall_setup.sh script if called during package installation.
    git-svn-id: https://xtreemfs.googlecode.com/svn/trunk@3507 f8528748-c2c0-11dd-ba5c-d3b7c2b3b534

--- packaging/postinstall_setup.sh
+++ packaging/postinstall_setup.sh
@@ -7,27 +7,35 @@ XTREEMFS_ETC=/etc/xos/xtreemfs
 XTREEMFS_USER=xtreemfs
 XTREEMFS_GROUP=xtreemfs
 
+# When executed during POST installation, do not be verbose.
+VERBOSE=0
+script_name=$(basename "$0")
+if [ "$script_name" = "postinstall_setup.sh" ]
+then
+  VERBOSE=1
+fi
+
 group_exists=`grep -c $XTREEMFS_GROUP /etc/group || true`
 if [ $group_exists -eq 0 ]; then
     groupadd $XTREEMFS_GROUP
-    echo "created group $XTREEMFS_GROUP"
+    [ $VERBOSE -eq 1 ] && echo "created group $XTREEMFS_GROUP"
 fi
 exists=`grep -c $XTREEMFS_USER /etc/passwd || true`
 if [ $exists -eq 0 ]; then
     mkdir $XTREEMFS_HOME
     useradd -r --home $XTREEMFS_HOME -g $XTREEMFS_GROUP $XTREEMFS_USER
     chown $XTREEMFS_USER $XTREEMFS_HOME
-    echo "created user $XTREEMFS_USER and data directory $XTREEMFS_HOME"
-else
-    if [ ! -d $XTREEMFS_HOME ]; then
-        mkdir $XTREEMFS_HOME
-        echo "user $XTREEMFS_USER exists but data directory $XTREEMFS_HOME had to be created"
-    fi
-    owner=`stat -c %U $XTREEMFS_HOME`
-    if [ $owner != $XTREEMFS_USER ]; then
-        echo "directory $XTREEMFS_HOME is not owned by $XTREEMFS_USER, executing chown (may take some time)"
-        chown -R $XTREEMFS_USER $XTREEMFS_HOME
-    fi
+    [ $VERBOSE -eq 1 ] && echo "created user $XTREEMFS_USER and data directory $XTREEMFS_HOME"
+fi
+if [ ! -d $XTREEMFS_HOME ]; then
+    mkdir -m750 $XTREEMFS_HOME
+    chown $XTREEMFS_USER $XTREEMFS_HOME
+    [ $VERBOSE -eq 1 ] && echo "user $XTREEMFS_USER exists but data directory $XTREEMFS_HOME had to be created"
+fi
+owner=`stat -c %U $XTREEMFS_HOME`
+if [ $owner != $XTREEMFS_USER ]; then
+    [ $VERBOSE -eq 1 ] && echo "directory $XTREEMFS_HOME is not owned by $XTREEMFS_USER, executing chown (may take some time)"
+    chown -R $XTREEMFS_USER $XTREEMFS_HOME
 fi
 
 if [ ! -e $XTREEMFS_LOG_DIR ]; then
@@ -36,20 +44,23 @@ if [ ! -e $XTREEMFS_LOG_DIR ]; then
 fi
 
 if [ -e $XTREEMFS_ETC ]; then
-    group=`stat -c %G $XTREEMFS_ETC`
+    group=`stat -c %G $XTREEMFS_ETC 2>/dev/null`
     if [ $group != $XTREEMFS_GROUP ]; then
-        echo "directory $XTREEMFS_ETC is owned by $group, should be owned by $XTREEMFS_GROUP, executing chgrp (may take some time)"
+        [ $VERBOSE -eq 1 ] && echo "directory $XTREEMFS_ETC is owned by $group, should be owned by $XTREEMFS_GROUP, executing chgrp (may take some time)"
         chgrp -R $XTREEMFS_GROUP $XTREEMFS_ETC
     fi
     for file in `ls $XTREEMFS_ETC/*.properties 2>/dev/null`; do
       if [ -f $file -a "$(stat -c %a $file)" != "640" ]; then
-          echo "setting $file 0640, executing chmod"
+          [ $VERBOSE -eq 1 ] && echo "setting $file 0640, executing chmod"
           chmod 0640 $file
       fi
     done
-    if [ -d "$XTREEMFS_ETC/truststore/" -a "$(stat -c %a "$XTREEMFS_ETC/truststore/")" != "750" ]
+    if [ -d "$XTREEMFS_ETC/truststore/" ]
     then
-        echo "setting $XTREEMFS_ETC/truststore/ to 0750, executing chmod (may take some time)"
-        chmod -R u=rwX,g=rX,o= $XTREEMFS_ETC/truststore/
+        if [ "$(stat -c %a "$XTREEMFS_ETC/truststore/")" != "750" ]
+        then
+            [ $VERBOSE -eq 1 ] && echo "setting $XTREEMFS_ETC/truststore/ to 0750, executing chmod (may take some time)"
+            chmod -R u=rwX,g=rX,o= $XTREEMFS_ETC/truststore/
+        fi
     fi
 fi
