e1ae22ffa8f1819e57669b8edd4b1f14d0a52b34
michael.berlin.xtreemfs@gmail.com <michael.berlin.xtreemfs@gmail.com@f8528748-c2c0-11dd-ba5c-d3b7c2b3b534>
2012-11-07 16:59:42 +0000

    tests: junit_tests.sh: - Enable assertions for JVM. - Create test directory and the subdirectory log if they do not exist. - Guess XTREEMFS directory if $1 was omitted. - Do not log the whole command line for compiling the tests because it is far too long.
    git-svn-id: https://xtreemfs.googlecode.com/svn/trunk@3546 f8528748-c2c0-11dd-ba5c-d3b7c2b3b534

--- tests/test_scripts/junit_tests.sh
+++ tests/test_scripts/junit_tests.sh
@@ -3,10 +3,20 @@
 # runs all JUnit tests in the subpackage 'org.xtreemfs.test' that end with '*Test.java'
 
 TEST_DIR=$4
-if [ -z $TEST_DIR ]; then TEST_DIR=/tmp/xtreemfs-junit; fi
+if [ -z $TEST_DIR ]
+then
+  TEST_DIR=/tmp/xtreemfs-junit
+  if [ ! -d "$TEST_DIR" ]; then mkdir "$TEST_DIR"; fi
+  if [ ! -d "${TEST_DIR}/log" ]; then mkdir "${TEST_DIR}/log"; fi
+fi
 echo "TEST_DIR: $TEST_DIR"
 
-export XTREEMFS=$1
+XTREEMFS=$1
+if [ -z "$XTREEMFS" ]
+then
+  # Try to guess the path
+  if [ -d "java/servers" ]; then XTREEMFS="."; fi
+fi
 echo "XTREEMFS=$XTREEMFS"
 
 # find all jars; build the classpath for running the JUnit tests
@@ -26,7 +36,8 @@ CLASSES_DIR=$TEST_DIR/classes
 # compile JUnit tests; store results in $CLASSES_DIR
 mkdir -p $CLASSES_DIR
 JAVAC_CALL="$JAVA_HOME/bin/javac -cp $CLASSPATH -d $TEST_DIR/classes $SOURCES"
-echo "Compiling tests: ${JAVAC_CALL}"
+echo "Compiling tests..."
+# echo "Compiling tests: ${JAVAC_CALL}"
 $JAVAC_CALL
 RESULT=$?
 if [ "$RESULT" -ne "0" ]; then echo "$COMMAND failed"; exit $RESULT; fi
@@ -60,7 +71,7 @@ while read LINE; do
 	fi
 
     # run each JUnit test separately in its own JVM
-    JAVA_CALL="$JAVA_HOME/bin/java -cp $CLASSPATH org.junit.runner.JUnitCore $TEST"
+    JAVA_CALL="$JAVA_HOME/bin/java -ea -cp $CLASSPATH org.junit.runner.JUnitCore $TEST"
 	
     echo -n "Running test `expr $COUNTER + 1`: $TEST ... "
     $JAVA_CALL >>$TEST_DIR/log/junit.log 2>&1
