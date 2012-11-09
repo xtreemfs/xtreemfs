d5aaafa025e69af9d19b3aa87acf97e8b1bd11bf
michael.berlin.xtreemfs@gmail.com <michael.berlin.xtreemfs@gmail.com@f8528748-c2c0-11dd-ba5c-d3b7c2b3b534>
2012-11-09 22:00:37 +0000

    client: FuseAdapter: Respect Options::enable_atime and do not set Fuse Option -o noatime if option is set to true.
    git-svn-id: https://xtreemfs.googlecode.com/svn/trunk@3619 f8528748-c2c0-11dd-ba5c-d3b7c2b3b534

--- cpp/src/fuse/fuse_adapter.cpp
+++ cpp/src/fuse/fuse_adapter.cpp
@@ -310,8 +310,9 @@ void FuseAdapter::Start(std::list<char*>* required_fuse_options) {
   required_fuse_options->push_back(
       strdup("-ouse_ino,readdir_ino"));
   #ifndef __sun
-  // TODO(mberlin): Respect options_->enable_atime.
+  if (!options_->enable_atime) {
     required_fuse_options->push_back(strdup("-onoatime"));
+  }
   #endif
   required_fuse_options->push_back(strdup(
       (string("-ofsname=xtreemfs@") + options_->xtreemfs_url).c_str()));
