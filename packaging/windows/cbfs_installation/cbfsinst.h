699103f53db17e50ca494177d41b8b32fae2e0ae
michael.berlin.xtreemfs@gmail.com <michael.berlin.xtreemfs@gmail.com@f8528748-c2c0-11dd-ba5c-d3b7c2b3b534>
2012-11-10 23:22:01 +0000

    packaging: Added InnoSetup installer for new Windows Client.
    git-svn-id: https://xtreemfs.googlecode.com/svn/trunk@3634 f8528748-c2c0-11dd-ba5c-d3b7c2b3b534

---
+++ packaging/windows/cbfs_installation/cbfsinst.h
@@ -0,0 +1,97 @@
+// The following ifdef block is the standard way of creating macros which make exporting 
+// from a DLL simpler. All files within this DLL are compiled with the CBFSISTALL_EXPORTS
+// symbol defined on the command line. this symbol should not be defined on any project
+// that uses this DLL. This way any other project whose source files include this file see 
+// CBFSISTALL_API functions as being imported from a DLL, whereas this DLL sees symbols
+// defined with this macro as being exported.
+#ifdef CBFSINST_EXPORTS
+#ifdef __cplusplus
+#define CBFSINST_API extern "C" __declspec(dllexport)
+#else
+#define CBFSINST_API __declspec(dllexport)
+#endif
+#else
+#define CBFSINST_API __declspec(dllimport)
+#endif
+
+#define CBFS_MODULE_DRIVER                 0x00000001
+#define CBFS_MODULE_NET_REDIRECTOR_DLL     0x00010000
+#define CBFS_MODULE_MOUNT_NOTIFIER_DLL     0x00020000
+
+CBFSINST_API BOOL __stdcall InstallA(
+    IN LPCSTR  CabPathName, 
+    IN LPCSTR  ProductName,
+		IN BOOL SupportPnP,
+		IN DWORD ModulesToInstall,
+    OUT LPDWORD RebootNeeded
+    );
+
+CBFSINST_API BOOL __stdcall InstallW(
+    IN LPCWSTR  CabPathName, 
+    IN LPCWSTR ProductName,
+		IN BOOL SupportPnP,
+    IN DWORD ModulesToInstall,
+    OUT LPDWORD RebootNeeded
+    );
+
+CBFSINST_API BOOL __stdcall UninstallA(
+    IN LPCSTR  CabPathName, 
+    IN LPCSTR   ProductName,
+    OUT LPDWORD  RebootNeeded
+    );
+
+CBFSINST_API BOOL __stdcall UninstallW(
+    IN LPCWSTR  CabPathName, 
+    IN LPCWSTR  ProductName,
+    OUT LPDWORD  RebootNeeded
+    );
+
+CBFSINST_API BOOL __stdcall GetModuleStatusA(
+    IN LPCSTR   ProductName,
+    IN DWORD    Module,
+    OUT LPBOOL  Installed,
+    OUT LPDWORD FileVersionHigh OPTIONAL,
+    OUT LPDWORD FileVersionLow OPTIONAL
+    );
+
+CBFSINST_API BOOL __stdcall GetModuleStatusW(
+    IN LPCWSTR  ProductName,
+    IN DWORD    Module,
+    OUT LPBOOL  Installed,
+    OUT LPDWORD FileVersionHigh OPTIONAL,
+    OUT LPDWORD FileVersionLow OPTIONAL
+    );
+/*
+CBFSINST_API BOOL __stdcall GetDriverVersionA(
+    IN LPCSTR DriverFile,
+    OUT LPDWORD FileVersionHigh,
+    OUT LPDWORD FileVersionLow
+    );
+
+CBFSINST_API BOOL __stdcall GetDriverVersionW(
+    IN LPCWSTR DriverFile,
+    OUT LPDWORD FileVersionHigh,
+    OUT LPDWORD FileVersionLow
+    );
+*/
+CBFSINST_API BOOL __stdcall InstallIconA(
+		IN LPCSTR IconPath,
+    IN LPCSTR IconId,
+    OUT LPBOOL  RebootNeeded
+    );
+
+CBFSINST_API BOOL __stdcall InstallIconW(
+		IN LPCWSTR IconPath,
+    IN LPCWSTR IconId,
+    OUT LPBOOL  RebootNeeded
+    );
+
+CBFSINST_API BOOL __stdcall UninstallIconA(
+    IN LPCSTR IconId,
+    OUT LPBOOL  RebootNeeded
+    );
+
+CBFSINST_API BOOL __stdcall UninstallIconW(
+    IN LPCWSTR IconId,
+    OUT LPBOOL  RebootNeeded
+    );
