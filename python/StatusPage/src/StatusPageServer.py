3b1eacf7cfcf6bca55dffc37d991547aca62db5c
Christoph Kleineweber <kleineweber@zib.de>
2013-02-27 15:36:47 +0100

    StatusPage: Initial version of status website for presenting host reachability

---
+++ python/StatusPage/src/StatusPageServer.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+import subprocess, threading, sys, time, BaseHTTPServer
+
+hosts = ["localhost", "www.google.de", "asdf"]
+hostStatus = {}
+hostStatusLock = threading.Lock()
+runThread = True
+
+def isReachable(host):
+    ret = subprocess.call("ping -w 1 -c 1 %s" % host, 
+        shell=True, 
+        stdout=open('/dev/null', 'w'), 
+        stderr=subprocess.STDOUT)
+    if ret == 0:
+        return True 
+    else:
+        return False
+
+class MonitoringThread(threading.Thread):
+    def __init__(self, hosts):
+        threading.Thread.__init__(self)
+        self.hosts = hosts
+
+    def run(self):
+        while runThread:
+            for host in self.hosts:
+                reachable = isReachable(host)
+                hostStatusLock.acquire()
+                hostStatus[host] = reachable
+                hostStatusLock.release()
+            time.sleep(0.5)
+
+class StatusHTTPRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+    def generateStatusOutput(self):
+        hostStatusLock.acquire()
+        result = "<table>"
+
+        for key in hostStatus.keys():
+            if hostStatus[key] == True:
+                color = "green"
+            else:
+                color = "red"
+            result = result + "<tr><td bgcolor=" + color + ">" + key + "</td></tr>"
+
+        result = result + "</table>"
+        hostStatusLock.release()
+        return result
+
+    def do_GET(self):
+        try:
+            if(self.path.endswith("status")):
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+                self.wfile.write(self.generateStatusOutput())
+                return
+            else:
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+                self.wfile.write("""
+                        <html>
+                            <head>
+                                <title>Host Status</title>
+                            </head>
+                            <script>
+                                function updateStatus() {
+                                    var ajaxRequest;
+
+                                    try {
+                                        ajaxRequest = new XMLHttpRequest();
+                                    } catch(e) {
+                                        console.log(e);
+                                    }
+                                    ajaxRequest.open('GET', 'status', true);
+                                    ajaxRequest.onreadystatechange = function() {
+                                        if (ajaxRequest.readyState == 4) {
+                                            var statusData = ajaxRequest.responseText;
+                                            var txt = document.getElementById("status");
+                                            txt.innerHTML = statusData;
+                                        }
+                                    };
+                                    ajaxRequest.send(null);
+                                    window.setTimeout(updateStatus, 1000);
+                                }
+                                updateStatus();
+                            </script>
+                            <body>
+                                <div id=status></div>
+                            </body>
+                        </html>
+                    """)
+                return
+        except IOError:
+            self.send_error(404, 'Error')
+
+def main():
+    try:
+        monitoringThread = MonitoringThread(hosts)
+        monitoringThread.start()
+        webserver = BaseHTTPServer.HTTPServer(('', 8080), StatusHTTPRequestHandler)
+        print 'starting webserver'
+        webserver.serve_forever()
+    except KeyboardInterrupt:
+        runThread = False
+        webserver.socket.close()
+        sys.exit(0)
+
+if __name__ == "__main__":
+    main()
