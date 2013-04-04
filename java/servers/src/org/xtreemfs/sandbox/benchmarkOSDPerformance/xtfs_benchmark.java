251769e39150ceff03bfc09c80e4a391dbd076d5
jensvfischer <jensvfischer@gmail.com>
2013-04-04 16:53:36 +0200

    Refactor CLI Interface to use Controller and Params
    Rename CLIParser to xtfs_benchmark.
    Rewrite xtfs_benchmark to use the Controller and Params.

---
+++ java/servers/src/org/xtreemfs/sandbox/benchmarkOSDPerformance/xtfs_benchmark.java
@@ -0,0 +1,334 @@
+package org.xtreemfs.sandbox.benchmarkOSDPerformance;
+
+import static org.xtreemfs.foundation.logging.Logging.*;
+import static org.xtreemfs.foundation.util.CLIParser.CliOption;
+import static org.xtreemfs.foundation.util.CLIParser.CliOption.OPTIONTYPE.STRING;
+import static org.xtreemfs.foundation.util.CLIParser.CliOption.OPTIONTYPE.SWITCH;
+import static org.xtreemfs.foundation.util.CLIParser.parseCLI;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+import org.xtreemfs.utils.DefaultDirConfig;
+import org.xtreemfs.utils.utils;
+
+public class xtfs_benchmark {
+    static final int                      KiB_IN_BYTES = 1024;
+    static final int                      MiB_IN_BYTES = 1024 * 1024;
+    static final int                      GiB_IN_BYTES = 1024 * 1024 * 1024;
+
+    private static Map<String, CliOption> options;
+    private static List<String>           arguments;
+    private static ParamsBuilder          builder;
+    private static Controller             controller;
+
+    static {
+        options = utils.getDefaultAdminToolOptions(true);
+        builder = new ParamsBuilder();
+        arguments = new ArrayList<String>(5);
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        parseCLIOptions(args);
+        displayUsageIfSet();
+        Params params = buildParams();
+        System.out.println(params);
+        controller = new Controller(params);
+        controller.tryConnection();
+        setupVolumes(params);
+        runBenchmarks(params);
+        controller.teardown();
+
+    }
+
+    private static void parseCLIOptions(String[] args) {
+        initOptions();
+        parseCLI(args, options, arguments);
+    }
+
+    private static void displayUsageIfSet() {
+        if (usageIsSet())
+            displayUsage();
+    }
+
+    private static Params buildParams() {
+        setNumberOfThreads();
+        setSequentialSize();
+        setRandomSize();
+        setBasefileSize();
+        setFileSize();
+        setDirAddress();
+        setUsername();
+        setGroup();
+        setOSDPassword();
+        setAuth();
+        setSSLOptions();
+        setOptions();
+        return builder.build();
+    }
+
+    private static void setupVolumes(Params params) throws Exception {
+        VolumeManager.init(params);
+        VolumeManager volumeManager = VolumeManager.getInstance();
+
+        if (arguments.size() == 0)
+            volumeManager.createDefaultVolumes(params.numberOfThreads);
+        else {
+            // ToDo add check to verify that the number of volumes is in accordence with the number of threads
+            String[] volumes = arguments.toArray(new String[arguments.size()]);
+            volumeManager.openVolumes(volumes);
+        }
+    }
+
+    private static void runBenchmarks(Params params) throws Exception {
+
+        ConcurrentLinkedQueue<BenchmarkResult> result;
+        ConcurrentLinkedQueue<BenchmarkResult> results = new ConcurrentLinkedQueue<BenchmarkResult>();
+
+        if (sequentialReadBenchmarkIsSet()) {
+            result = controller.startBenchmarks(BenchmarkType.WRITE, params.numberOfThreads);
+            results.addAll(result);
+        }
+
+        if (sequentialWriteBenchmarkIsSet()) {
+            result = controller.startBenchmarks(BenchmarkType.READ, params.numberOfThreads);
+            results.addAll(result);
+        }
+
+        if (randomReadBenchmarkIsSet()) {
+            result = controller.startBenchmarks(BenchmarkType.RANDOM_IO_READ, 1);
+            results.addAll(result);
+        }
+
+        if (randomFilebasedWriteBenchmarkIsSet()) {
+            result = controller.startBenchmarks(BenchmarkType.RANDOM_IO_WRITE_FILEBASED, 1);
+            results.addAll(result);
+        }
+
+        if (randomFilebasedReadBenchmarkIsSet()) {
+            result = controller.startBenchmarks(BenchmarkType.RANDOM_IO_READ_FILEBASED, 1);
+            results.addAll(result);
+        }
+
+        Controller.printResults(results);
+    }
+
+    private static void initOptions() {
+
+        /*
+         * -sw -sr -rw -p -rr -rfr -rfw -p <number> -ssize -rsize -file-size -basefile-size -no-cleanup
+         * volume1 volume2
+         */
+
+        /* Connection Data */
+        options.put("-dir-address", new CliOption(STRING,
+                "directory service to use (e.g. 'localhost:32638'). If no URI is specified, URI and security settings are taken from '"
+                        + DefaultDirConfig.DEFAULT_DIR_CONFIG + "'", "<uri>"));
+        options.put("-username", new CliOption(STRING, "username to use", "<username>"));
+
+        /* benchmark switches */
+        options.put("sw", new CliOption(SWITCH, "sequential write benchmark", ""));
+        options.put("sr", new CliOption(SWITCH, "sequential read benchmark", ""));
+        options.put("rw", new CliOption(SWITCH, "random write benchmark", ""));
+        options.put("rr", new CliOption(SWITCH, "random read benchmark", ""));
+        options.put("rfw", new CliOption(SWITCH, "random filebased write benchmark", ""));
+        options.put("rfr", new CliOption(SWITCH, "random filebased read benchmark", ""));
+
+        options.put("p",
+                new CliOption(STRING, "number of sequential benchmarks to be started in parallel. default: 1", "<number>"));
+
+        /* sizes */
+        options.put("ssize", new CliOption(STRING,
+                "size for sequential benchmarks in [B|K|M|G] (no modifier assumes bytes)", "<size>"));
+        options.put("rsize", new CliOption(STRING,
+                "size for random benchmarks in [B|K|M|G] (no modifier assumes bytes)", "<size>"));
+        options.put("-basefile-size", new CliOption(STRING,
+                "size of the basefile for random benchmarks in [B|K|M|G] (no modifier assumes bytes)", "<size>"));
+        options.put("-file-size", new CliOption(STRING,
+                "size of the files for random filebased benchmarks in [B|K|M|G] (no modifier assumes bytes)."
+                        + " The filesize for filebased random IO Benchmarks must be <= 23^31-1", "<size>"));
+
+        // todo: implement deletion options
+        /* deletion options */
+        options.put("-no-cleanup", new CliOption(SWITCH, "do not delete created volumes and files", ""));
+        options.put("-no-cleanup-volumes", new CliOption(SWITCH, "do not delete created volumes", ""));
+        options.put("-no-cleanup-files", new CliOption(SWITCH, "do not delete created files", ""));
+    }
+
+    private static boolean usageIsSet() {
+        return (options.get(utils.OPTION_HELP).switchValue || options.get(utils.OPTION_HELP_LONG).switchValue);
+    }
+
+    /**
+     * Prints out displayUsage informations
+     */
+    private static void displayUsage() {
+
+        System.out.println("\nusage: xtfs_benchmark [options] volume1 volume2 ... \n");
+        System.out
+                .println("The number of volumes must be in accordance with the number of benchmarks run in parallel (see -p).");
+        System.out
+                .println("All sizes can be modified with multiplication modifiers, where K means KiB, M means MiB and G means GiB. \nIf no modifier is given, sizes are assumed to be in bytes.");
+        System.out.println();
+        System.out.println("  " + "options:");
+        utils.printOptions(options);
+        System.out.println();
+        System.out.println("example: xtfs_benchmark -sw -sr -p 3 -ssize 3G volume1 volume2 volume3");
+        System.out
+                .println("\t\t starts a sequential write and read benchmark of 3 GiB with 3 benchmarks in parallel on volume1, volume2 and volume3\n");
+    }
+
+    private static void setNumberOfThreads() {
+        String optionValue = options.get("p").stringValue;
+        if (null != optionValue)
+            builder.setNumberOfThreads(Integer.valueOf(optionValue));
+    }
+
+    private static void setSequentialSize() {
+        String optionValue;
+        optionValue = options.get("ssize").stringValue;
+        if (null != optionValue)
+            builder.setSequentialSizeInBytes(parseSizeToBytes(optionValue));
+    }
+
+    private static void setRandomSize() {
+        String optionValue;
+        optionValue = options.get("rsize").stringValue;
+        if (null != optionValue)
+            builder.setRandomSizeInBytes(parseSizeToBytes(optionValue));
+    }
+
+    private static void setBasefileSize() {
+        String optionValue;
+        optionValue = options.get("-basefile-size").stringValue;
+        if (null != optionValue)
+            builder.setBasefileSizeInBytes(parseSizeToBytes(optionValue));
+    }
+
+    private static void setFileSize() {
+        String optionValue;
+        optionValue = options.get("-file-size").stringValue;
+        if (null != optionValue) {
+            long sizeInBytes = parseSizeToBytes(optionValue);
+            if (sizeInBytes > Integer.MAX_VALUE)
+                throw new IllegalArgumentException("Filesize for filebased random IO Benchmarks must be <= 23^31-1");
+            builder.setRandomIOFilesize((int) sizeInBytes);
+        }
+    }
+
+    /* Use DirAdress from Param, if not present, use DirAddress from ConfigFile, if not preset, use Default */
+    private static void setDirAddress() {
+        String dirAddressFromCLI = options.get("-dir-address").stringValue;
+        String dirAddressFromConfig = getDefaultDir();
+        if (null != dirAddressFromCLI)
+            builder.setDirAddress(dirAddressFromCLI);
+        else if (null != dirAddressFromConfig)
+            builder.setDirAddress(dirAddressFromConfig);
+    }
+
+    private static void setUsername() {
+        String userName = options.get("-username").stringValue;
+        if (null != userName)
+            builder.setUserName(userName);
+    }
+
+    private static void setGroup() {
+        // Todo Replace with real setGroup()
+        String userName = options.get("-username").stringValue;
+        if (null != userName)
+            builder.setGroup(userName);
+    }
+
+    private static void setOSDPassword() {
+        String osdPassword = options.get(utils.OPTION_ADMIN_PASS).stringValue;
+        if (null != osdPassword)
+            builder.setOsdPassword(osdPassword);
+    }
+
+    private static void setAuth() {
+        // Todo (jvf) implement?
+    }
+
+    private static void setSSLOptions() {
+        // Todo (jvf) Implement SSL Options?
+    }
+
+    private static void setOptions() {
+        // Todo (jvf) implement?
+    }
+
+    private static boolean sequentialWriteBenchmarkIsSet() {
+        return options.get("sw").switchValue;
+    }
+
+    private static boolean sequentialReadBenchmarkIsSet() {
+        return options.get("sr").switchValue;
+    }
+
+    private static boolean randomReadBenchmarkIsSet() {
+        return options.get("rr").switchValue;
+    }
+
+    private static boolean randomFilebasedWriteBenchmarkIsSet() {
+        return options.get("rfw").switchValue;
+    }
+
+    private static boolean randomFilebasedReadBenchmarkIsSet() {
+        return options.get("rfr").switchValue;
+    }
+
+    private static long parseSizeToBytes(String size) {
+        size = size.toUpperCase();
+        long sizeInBytes;
+
+        if (!size.matches("[0-9]+[BKMG]?"))
+            throw new IllegalArgumentException("Wrong format for size.");
+
+        if (size.matches("[0-9]+")) {
+            sizeInBytes = Long.valueOf(size);
+        } else {
+            char suffix = size.charAt(size.length() - 1);
+            long numbers = Long.valueOf(size.substring(0, size.length() - 1));
+            switch (suffix) {
+            case 'B':
+                sizeInBytes = numbers;
+                break;
+            case 'K':
+                sizeInBytes = numbers * (long) KiB_IN_BYTES;
+                break;
+            case 'M':
+                sizeInBytes = numbers * (long) MiB_IN_BYTES;
+                break;
+            case 'G':
+                sizeInBytes = numbers * (long) GiB_IN_BYTES;
+                break;
+            default:
+                sizeInBytes = 0L;
+                break;
+            }
+        }
+        return sizeInBytes;
+    }
+
+    // Todo (jvf) Move to Controller or Params?
+    private static String getDefaultDir() {
+        String[] dirAddresses;
+        DefaultDirConfig cfg = null;
+        try {
+            cfg = new DefaultDirConfig();
+            dirAddresses = cfg.getDirectoryServices();
+            return dirAddresses[0];
+        } catch (IOException e) {
+            logMessage(LEVEL_ERROR, Category.tool, xtfs_benchmark.class,
+                    "Could not read Default DIR Config in %s. Errormessage: %s", DefaultDirConfig.DEFAULT_DIR_CONFIG,
+                    e.getMessage());
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+}
