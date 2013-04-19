aacea3289aaed6c7ab1138c103fef053db83c084
Christoph Kleineweber <kleineweber@zib.de>
2013-04-19 16:48:14 +0200

    QoS: Added template for scheduler status page

--- java/servers/src/org/xtreemfs/scheduler/SchedulerStatusPage.java
+++ java/servers/src/org/xtreemfs/scheduler/SchedulerStatusPage.java
@@ -8,10 +8,46 @@
 
 package org.xtreemfs.scheduler;
 
+import org.xtreemfs.foundation.logging.Logging;
+import org.xtreemfs.foundation.util.OutputUtils;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 /**
  * @author Christoph Kleineweber <kleineweber@zib.de>
  */
 public class SchedulerStatusPage {
+    private final static String  statusPageTemplate;
+
+    static {
+        StringBuffer sb = null;
+        try {
+            InputStream is = SchedulerStatusPage.class.getClassLoader().getResourceAsStream(
+                    "org/xtreemfs/scheduler/templates/status.html");
+            if (is == null) {
+                is = SchedulerStatusPage.class.getClass().getResourceAsStream("../templates/status.html");
+            }
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            sb = new StringBuffer();
+            String line = br.readLine();
+            while (line != null) {
+                sb.append(line + "\n");
+                line = br.readLine();
+            }
+            br.close();
+        } catch (Exception ex) {
+            Logging.logMessage(Logging.LEVEL_WARN, Logging.Category.misc, (Object) null,
+                    "could not load status page template: %s", OutputUtils.stackTraceToString(ex));
+        }
+        if (sb == null) {
+            statusPageTemplate = "<H1>Template was not found, unable to show status page!</h1>";
+        } else {
+            statusPageTemplate = sb.toString();
+        }
+    }
+
     public static String getStatusPage(SchedulerRequestDispatcher master) {
         StringBuilder dump = new StringBuilder();
 
