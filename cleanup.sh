4743b8c7a6b72f321c4d70a9cfb4541d46801e8b
bzcfisch <bzcfisch@alibaba.zib.de>
2013-05-06 12:30:30 +0200

    Add cleanup script

---
+++ cleanup.sh
@@ -0,0 +1,18 @@
+#/bin/sh
+
+./bin/xtfs_cleanup -e -dir pbrpc://localhost:32638/ uuid:bench.osd01 &
+./bin/xtfs_cleanup -e -dir pbrpc://localhost:32638/ uuid:bench.osd02 &
+./bin/xtfs_cleanup -e -dir pbrpc://localhost:32638/ uuid:bench.osd03 &
+./bin/xtfs_cleanup -e -dir pbrpc://localhost:32638/ uuid:bench.osd04 &
+./bin/xtfs_cleanup -e -dir pbrpc://localhost:32638/ uuid:bench.osd05 &
+./bin/xtfs_cleanup -e -dir pbrpc://localhost:32638/ uuid:bench.osd06 &
+./bin/xtfs_cleanup -e -dir pbrpc://localhost:32638/ uuid:bench.osd07 &
+./bin/xtfs_cleanup -e -dir pbrpc://localhost:32638/ uuid:bench.osd08 &
+./bin/xtfs_cleanup -e -dir pbrpc://localhost:32638/ uuid:bench.osd09 &
+./bin/xtfs_cleanup -e -dir pbrpc://localhost:32638/ uuid:bench.osd10 &
+./bin/xtfs_cleanup -e -dir pbrpc://localhost:32638/ uuid:bench.osd11 &
+./bin/xtfs_cleanup -e -dir pbrpc://localhost:32638/ uuid:bench.osd12 &
+./bin/xtfs_cleanup -e -dir pbrpc://localhost:32638/ uuid:bench.osd13 &
+./bin/xtfs_cleanup -e -dir pbrpc://localhost:32638/ uuid:bench.osd14 &
+./bin/xtfs_cleanup -e -dir pbrpc://localhost:32638/ uuid:bench.osd15 &
+./bin/xtfs_cleanup -e -dir pbrpc://localhost:32638/ uuid:bench.osd16
