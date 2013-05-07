b599f0bbf7f38a2acc91dab3efb2dd02aad72348
Christoph Kleineweber <kleineweber@zib.de>
2013-05-07 13:53:23 +0200

    QoS: Extended ReservationStoreTest

--- java/servers/test/org/xtreemfs/scheduler/data/store/ReservationStoreTest.java
+++ java/servers/test/org/xtreemfs/scheduler/data/store/ReservationStoreTest.java
@@ -1,5 +1,7 @@
 package org.xtreemfs.scheduler.data.store;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.xtreemfs.babudb.BabuDBFactory;
 import org.xtreemfs.babudb.api.BabuDB;
@@ -19,40 +21,55 @@ public class ReservationStoreTest {
 	BabuDBConfig dbsConfig;
 	private BabuDB database;
 	final String DB_NAME = "schedtest";
-	
+    DatabaseManager dbm;
+    Database db;
+    Reservation r1 = new Reservation("testVolume", ReservationType.STREAMING_RESERVATION, 0.0, 100.0, 100.0);
+
+    @Before
+    public void setUp() throws Exception {
+        // Setting up database and ReservationStore
+        this.dbsConfig = SetupUtils.createSchedulerdbsConfig();
+        database = BabuDBFactory.createBabuDB(dbsConfig);
+        dbm = database.getDatabaseManager();
+
+        try {
+            db = dbm.createDatabase(DB_NAME, 2);
+        } catch (BabuDBException ex) {
+            db = dbm.getDatabase(DB_NAME);
+        }
+        store = new ReservationStore(db, 0);
+
+        store.storeReservation(r1);
+        assertEquals(store.getReservations().size(), 1);
+        assertTrue(store.getReservations().contains(r1));
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        // Cleanup
+        database.getDatabaseManager().deleteDatabase(DB_NAME);
+        database.shutdown();
+    }
+
 	@Test
 	public void testStoreReservation() throws Exception {
-		// Setting up database and ReservationStore
-		this.dbsConfig = SetupUtils.createSchedulerdbsConfig();
-		database = BabuDBFactory.createBabuDB(dbsConfig);
-		DatabaseManager dbm = database.getDatabaseManager();
-		Database db;
-		try {
-			db = dbm.createDatabase(DB_NAME, 2);
-		} catch (BabuDBException ex) {
-			db = dbm.getDatabase(DB_NAME);
-		}
-		store = new ReservationStore(db, 0);
-		
-		// Test writing and reading reservations
-		Reservation r1 = new Reservation("testVolume", ReservationType.STREAMING_RESERVATION, 0.0, 100.0, 100.0);
-		store.storeReservation(r1);
 		assertEquals(store.getReservations().size(), 1);
 		assertTrue(store.getReservations().contains(r1));
-		
-		// Test restoring reservations from database
-		store = null;
-		store = new ReservationStore(db, 0);
-		
-		assertEquals(store.getReservations().size(), 1);
-		assertTrue(store.getReservations().contains(r1));
-		
-		// Test deleting reservations
-		store.removeReservation(r1);
-		assertEquals(store.getReservations().size(), 0);
-		
-		// Cleanup
-		database.getDatabaseManager().deleteDatabase(DB_NAME);
-		database.shutdown();
 	}
+
+    @Test
+    public void testRemoveReservation() throws Exception {
+        // Test deleting reservations
+        store.removeReservation(r1);
+        assertEquals(store.getReservations().size(), 0);
+    }
+
+    @Test
+    public void testRestoreReservation()  throws Exception {
+        store = null;
+        store = new ReservationStore(db, 0);
+
+        assertEquals(store.getReservations().size(), 1);
+        assertTrue(store.getReservations().contains(r1));
+    }
 }
