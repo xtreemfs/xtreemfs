496447b73e29d2647484c7ea6a3440acb4c2693c
Christoph Kleineweber <kleineweber@zib.de>
2013-05-17 16:39:13 +0200

    QoS: Fixed osd exploration

--- java/servers/src/org/xtreemfs/scheduler/SchedulerRequestDispatcher.java
+++ java/servers/src/org/xtreemfs/scheduler/SchedulerRequestDispatcher.java
@@ -278,7 +278,7 @@ public class SchedulerRequestDispatcher extends LifeCycleThread implements
 
             if(!osdFound) {
                 try {
-                    byte[] bytes = getSchedulerDatabase().lookup(INDEX_ID_OSDS, osd.getUuid().getBytes(), null).get();
+                    byte[] bytes = getSchedulerDatabase().lookup(INDEX_ID_OSDS, osd.getName().getBytes(), null).get();
                     if(bytes != null) {
                         OSDDescription osdDescription = new OSDDescription(bytes);
                         getOsds().add(osdDescription);
@@ -300,10 +300,10 @@ public class SchedulerRequestDispatcher extends LifeCycleThread implements
 				
 				// TODO(ckleineweber): Determine osd type
 				OSDDescription.OSDType type = OSDDescription.OSDType.UNKNOWN;
-				OSDDescription osdDescription = new OSDDescription(osd.getUuid(), osdPerf, type);
+				OSDDescription osdDescription = new OSDDescription(osd.getName(), osdPerf, type);
 				getOsds().add(osdDescription);
 				try {
-					getSchedulerDatabase().singleInsert(INDEX_ID_OSDS, osd.toByteArray(), osdDescription.getBytes(), null);
+					getSchedulerDatabase().singleInsert(INDEX_ID_OSDS, osd.getName().getBytes(), osdDescription.getBytes(), null).get();
 				} catch(BabuDBException ex) {
 					Logging.logError(Logging.LEVEL_ERROR, this, ex);
 				}
