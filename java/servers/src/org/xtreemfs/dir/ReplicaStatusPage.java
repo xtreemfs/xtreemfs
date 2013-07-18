xtreemfs/xtreemfs

commit 9973c6a3c0dfec9f7efdb27b474d675bbf88db14

server: DIR: webinterface: Do not show OSDs on the replica status which were shutdown i.e., their last_updated_s value is set to 0.

java/servers/src/org/xtreemfs/dir/ReplicaStatusPage.java
@@ -43,6 +43,7 @@ public class ReplicaStatusPage extends StatusServerModule {
             this.template = template;
         }
 
+        @Override
         public String toString() {
             return template;
         }
@@ -125,8 +126,8 @@ public class ReplicaStatusPage extends StatusServerModule {
                         }
                     }
 
-                    // Add only OSDs with a status_page_url
-                    if (data.containsKey("status_page_url")) {
+                    // Add only OSDs with a status_page_url and are not shutdown (last_updated_s == 0).
+                    if (data.containsKey("status_page_url") && sreg.getLast_updated_s() > 0) {
                         osds.put(sreg.getUuid(), data);
                     }
                 }

commit 21f496ffdd524118ad737d2605f703a57727c2c8

server: DIR: webinterface: Do not show OSDs on the replica status which were shutdown i.e., whose last_updated_s value is set to 0.
Fixed inconsistency between comment and code introduced in revision 9973c6a3c0df.

java/servers/src/org/xtreemfs/dir/ReplicaStatusPage.java
@@ -126,8 +126,8 @@ public class ReplicaStatusPage extends StatusServerModule {
                         }
                     }
 
-                    // Add only OSDs with a status_page_url and are not shutdown (last_updated_s == 0).
-                    if (data.containsKey("status_page_url") && sreg.getLast_updated_s() > 0) {
+                    // Add only OSDs with a status_page_url and are not shutdown.
+                    if (data.containsKey("status_page_url") && sreg.getLast_updated_s() != 0) {
                         osds.put(sreg.getUuid(), data);
                     }
                 }