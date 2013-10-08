f4ae3d10f5c9d23ae0360793c050fe08d93c2306
jensvfischer <jensvfischer@gmail.com>
2013-10-08 22:31:01 +0200

    server: benchmark: Make BenchmarkUtils public

--- java/servers/src/org/xtreemfs/common/benchmark/BenchmarkUtils.java
+++ java/servers/src/org/xtreemfs/common/benchmark/BenchmarkUtils.java
@@ -14,16 +14,16 @@ package org.xtreemfs.common.benchmark;
  * @author jensvfischer
  *
  */
-class BenchmarkUtils {
+public class BenchmarkUtils {
 
-    static final int KiB_IN_BYTES = 1024;
-    static final int MiB_IN_BYTES = 1024 * 1024;
-    static final int GiB_IN_BYTES = 1024 * 1024 * 1024;
+    public static final int KiB_IN_BYTES = 1024;
+    public static final int MiB_IN_BYTES = 1024 * 1024;
+    public static final int GiB_IN_BYTES = 1024 * 1024 * 1024;
 
     /**
      * Enum for the different benchmark Types.
      */
-    static enum BenchmarkType {
+    public static enum BenchmarkType {
         SEQ_WRITE, SEQ_READ, RAND_WRITE, RAND_READ, FILES_WRITE, FILES_READ
     }
 }
