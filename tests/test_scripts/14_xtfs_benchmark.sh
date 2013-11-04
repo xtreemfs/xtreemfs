95bef267e40bda30b164f50440d170efdc4274a8
jensvfischer <jensvfischer@gmail.com>
2013-11-04 20:01:12 +0100

    jens-benchmark/server: Addjust sizes for xtfs_benchmark in xtestenv

--- tests/test_scripts/14_xtfs_benchmark.sh
+++ tests/test_scripts/14_xtfs_benchmark.sh
@@ -9,7 +9,7 @@ TEST_DIR=$4
 VOLUME="$(basename $(dirname $(pwd)))"
 
 exec $JAVA_HOME/bin/java -ea -cp $XTREEMFS/java/servers/dist/XtreemFS.jar:$XTREEMFS/java/foundation/dist/Foundation.jar:$XTREEMFS/java/lib/*:/usr/share/java/XtreemFS.jar:/usr/share/java/protobuf-java-2.3.0.jar:/usr/share/java/Foundation.jar:. \
-  org.xtreemfs.utils.xtfs_benchmark.xtfs_benchmark -sw -sr -rw -rr -fw -fr --stripe-size 128K --stripe-width 1 -ssize 1g -rsize 100m --file-size 4K --basefile-size 3g --dir-address $DIR_SERVER $VOLUME
+  org.xtreemfs.utils.xtfs_benchmark.xtfs_benchmark -sw -sr -rw -rr -fw -fr --stripe-size 128K --stripe-width 1 -ssize 500m -rsize 100m --file-size 4K --basefile-size 500m --dir-address $DIR_SERVER --user $USER --group $USER $VOLUME
 
 # minimal version for testing
 # exec $JAVA_HOME/bin/java -ea -cp $XTREEMFS/java/servers/dist/XtreemFS.jar:$XTREEMFS/java/foundation/dist/Foundation.jar:$XTREEMFS/java/lib/*:/usr/share/java/XtreemFS.jar:/usr/share/java/protobuf-java-2.3.0.jar:/usr/share/java/Foundation.jar:. \
