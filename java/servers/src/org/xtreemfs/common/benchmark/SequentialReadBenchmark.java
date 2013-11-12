39042752076e280d497c7810d13c1a18850f73d5
jensvfischer <jensvfischer@gmail.com>
2013-11-12 19:23:08 +0100

    jens-benchmark/server: Add special error handling for fileHandles

--- java/servers/src/org/xtreemfs/common/benchmark/SequentialReadBenchmark.java
+++ java/servers/src/org/xtreemfs/common/benchmark/SequentialReadBenchmark.java
@@ -36,6 +36,18 @@ class SequentialReadBenchmark extends SequentialBenchmark {
     long performIO(byte[] data, long numberOfBlocks) throws IOException {
         FileHandle fileHandle = volume.openFile(config.getUserCredentials(), filenames[0],
                 GlobalTypes.SYSTEM_V_FCNTL.SYSTEM_V_FCNTL_H_O_RDONLY.getNumber());
+        try {
+            return tryPerformIO(data, numberOfBlocks, fileHandle);
+        } catch (IOException e) {
+            /* closing the filehandle manually seems to be the only way to avoid an AssertionError in
+             * VolumeImplementation.internalShutdown() when shutting down client */
+            fileHandle.close();
+            throw e;
+        }
+    }
+
+
+    private long tryPerformIO(byte[] data, long numberOfBlocks, FileHandle fileHandle) throws IOException {
         long byteCounter = 0;
         for (long j = 0; j < numberOfBlocks; j++) {
             long nextOffset = j * chunkSize;
