f983b0eb139808355c9fce4e05ca7aa6d1647cd3
Michael Berlin <michael.berlin.xtreemfs@gmail.com>
2014-02-09 22:31:45 +0100

    foundation: BufferPool unit test: Fixed problem that each test assumed that the buffer pool was initially empty.

--- java/foundation/test/org/xtreemfs/foundation/buffer/BufferPoolTest.java
+++ java/foundation/test/org/xtreemfs/foundation/buffer/BufferPoolTest.java
@@ -11,82 +11,92 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+/**
+ * Tests different sequences of {@link BufferPool#allocate(int)} and {@link BufferPool#free(ReusableBuffer)}
+ * operations, some also regarding view buffers.
+ * 
+ * Please note that {@link BufferPool} is a singleton and therefore the pool size increases as the number of
+ * run tests does. Therefore, each test has to evaluate changes in the pool size relative to the pool size at
+ * the start of test.
+ * 
+ */
 public class BufferPoolTest {
     public static final int TEST_BUFFER_SIZE = 8192;
 
     @Test
     public final void testSimpleAllocateAndFree() {
         ReusableBuffer buf = null;
-
-        assertEquals(
-                "BufferPool must be empty because no buffers were returned yet and there nothing can be pooled yet.",
-                0, BufferPool.getPoolSize(TEST_BUFFER_SIZE));
+        // There may be already a buffer pooled. If not, the pool size will stay 0 after an allocate().
+        int currentPoolSize = Math.max(0, BufferPool.getPoolSize(TEST_BUFFER_SIZE) - 1);
 
         buf = BufferPool.allocate(TEST_BUFFER_SIZE);
-        assertEquals("BufferPool must be empty because a buffer was only allocated but not returned so far.", 0,
+        assertEquals("BufferPool must be empty because a buffer was only allocated but not returned so far.",
+                currentPoolSize,
                 BufferPool.getPoolSize(TEST_BUFFER_SIZE));
 
         BufferPool.free(buf);
-        assertEquals("One buffer must have been returned and pooled.", 1, BufferPool.getPoolSize(TEST_BUFFER_SIZE));
+        assertEquals("One buffer must have been returned and pooled.", currentPoolSize + 1,
+                BufferPool.getPoolSize(TEST_BUFFER_SIZE));
 
         buf = BufferPool.allocate(TEST_BUFFER_SIZE);
-        assertEquals("Pooled buffer must have been re-allocated.", 0, BufferPool.getPoolSize(TEST_BUFFER_SIZE));
+        assertEquals("Pooled buffer must have been re-allocated.", currentPoolSize,
+                BufferPool.getPoolSize(TEST_BUFFER_SIZE));
 
         BufferPool.free(buf);
-        assertEquals("One buffer must have been returned and pooled again.", 1,
+        assertEquals("One buffer must have been returned and pooled again.", currentPoolSize + 1,
                 BufferPool.getPoolSize(TEST_BUFFER_SIZE));
     }
 
     @Test
     public final void testReusableViewBuffers() {
         ReusableBuffer buf = null;
-
-        assertEquals(0, BufferPool.getPoolSize(TEST_BUFFER_SIZE));
+        // There may be already a buffer pooled. If not, the pool size will stay 0 after an allocate().
+        int currentPoolSize = Math.max(0, BufferPool.getPoolSize(TEST_BUFFER_SIZE) - 1);
 
         buf = BufferPool.allocate(TEST_BUFFER_SIZE);
-        assertEquals(0, BufferPool.getPoolSize(TEST_BUFFER_SIZE));
+        assertEquals(currentPoolSize, BufferPool.getPoolSize(TEST_BUFFER_SIZE));
 
         BufferPool.free(buf);
-        assertEquals(1, BufferPool.getPoolSize(TEST_BUFFER_SIZE));
+        assertEquals(currentPoolSize + 1, BufferPool.getPoolSize(TEST_BUFFER_SIZE));
 
         buf = BufferPool.allocate(TEST_BUFFER_SIZE);
-        assertEquals(0, BufferPool.getPoolSize(TEST_BUFFER_SIZE));
+        assertEquals(currentPoolSize, BufferPool.getPoolSize(TEST_BUFFER_SIZE));
         ReusableBuffer viewBuffer = buf.createViewBuffer();
 
         BufferPool.free(viewBuffer);
-        assertEquals("Buffer not returned to pool yet since one reference is left.", 0,
+        assertEquals("Buffer not returned to pool yet since one reference is left.", currentPoolSize,
                 BufferPool.getPoolSize(TEST_BUFFER_SIZE));
         BufferPool.free(buf);
-        assertEquals("Buffer must have been returned to pool since no reference is left.", 1,
+        assertEquals("Buffer must have been returned to pool since no reference is left.", currentPoolSize + 1,
                 BufferPool.getPoolSize(TEST_BUFFER_SIZE));
     }
 
     @Test
     public final void testReusableViewBuffersOfReusableViewBuffers() {
         ReusableBuffer buf = null;
-
-        assertEquals(0, BufferPool.getPoolSize(TEST_BUFFER_SIZE));
+        // There may be already a buffer pooled. If not, the pool size will stay 0 after an allocate().
+        int currentPoolSize = Math.max(0, BufferPool.getPoolSize(TEST_BUFFER_SIZE) - 1);
 
         buf = BufferPool.allocate(TEST_BUFFER_SIZE);
-        assertEquals(0, BufferPool.getPoolSize(TEST_BUFFER_SIZE));
+        assertEquals(currentPoolSize, BufferPool.getPoolSize(TEST_BUFFER_SIZE));
 
         BufferPool.free(buf);
-        assertEquals(1, BufferPool.getPoolSize(TEST_BUFFER_SIZE));
+        assertEquals(currentPoolSize + 1, BufferPool.getPoolSize(TEST_BUFFER_SIZE));
 
         buf = BufferPool.allocate(TEST_BUFFER_SIZE);
-        assertEquals(0, BufferPool.getPoolSize(TEST_BUFFER_SIZE));
+        assertEquals(currentPoolSize, BufferPool.getPoolSize(TEST_BUFFER_SIZE));
         ReusableBuffer viewBuffer = buf.createViewBuffer();
         // Create a view buffer of a view buffer.
         ReusableBuffer viewBuffer2 = viewBuffer.createViewBuffer();
 
         BufferPool.free(viewBuffer2);
-        assertEquals("Buffer not returned to pool yet since two references are left.", 0,
+        assertEquals("Buffer not returned to pool yet since two references are left.", currentPoolSize,
                 BufferPool.getPoolSize(TEST_BUFFER_SIZE));
         BufferPool.free(viewBuffer);
-        assertEquals("Buffer not returned to pool yet since one reference is left.", 0,
+        assertEquals("Buffer not returned to pool yet since one reference is left.", currentPoolSize,
                 BufferPool.getPoolSize(TEST_BUFFER_SIZE));
         BufferPool.free(buf);
-        assertEquals("Buffer must have been returned to pool since no reference is left.", 1,
+        assertEquals("Buffer must have been returned to pool since no reference is left.", currentPoolSize + 1,
                 BufferPool.getPoolSize(TEST_BUFFER_SIZE));
     }
 
@@ -102,14 +112,14 @@ public class BufferPoolTest {
         assertThatAssertionsAreEnabled();
 
         ReusableBuffer buf = null;
-
-        assertEquals(0, BufferPool.getPoolSize(TEST_BUFFER_SIZE));
+        // There may be already a buffer pooled. If not, the pool size will stay 0 after an allocate().
+        int currentPoolSize = Math.max(0, BufferPool.getPoolSize(TEST_BUFFER_SIZE) - 1);
 
         buf = BufferPool.allocate(TEST_BUFFER_SIZE);
-        assertEquals(0, BufferPool.getPoolSize(TEST_BUFFER_SIZE));
+        assertEquals(currentPoolSize, BufferPool.getPoolSize(TEST_BUFFER_SIZE));
 
         BufferPool.free(buf);
-        assertEquals(1, BufferPool.getPoolSize(TEST_BUFFER_SIZE));
+        assertEquals(currentPoolSize + 1, BufferPool.getPoolSize(TEST_BUFFER_SIZE));
 
         // Double free will trigger assertion.
         BufferPool.free(buf);
@@ -120,17 +130,17 @@ public class BufferPoolTest {
         assertThatAssertionsAreEnabled();
 
         ReusableBuffer buf = null;
-
-        assertEquals(0, BufferPool.getPoolSize(TEST_BUFFER_SIZE));
+        // There may be already a buffer pooled. If not, the pool size will stay 0 after an allocate().
+        int currentPoolSize = Math.max(0, BufferPool.getPoolSize(TEST_BUFFER_SIZE) - 1);
 
         buf = BufferPool.allocate(TEST_BUFFER_SIZE);
-        assertEquals(0, BufferPool.getPoolSize(TEST_BUFFER_SIZE));
+        assertEquals(currentPoolSize, BufferPool.getPoolSize(TEST_BUFFER_SIZE));
 
         BufferPool.free(buf);
-        assertEquals(1, BufferPool.getPoolSize(TEST_BUFFER_SIZE));
+        assertEquals(currentPoolSize + 1, BufferPool.getPoolSize(TEST_BUFFER_SIZE));
 
         buf = BufferPool.allocate(TEST_BUFFER_SIZE);
-        assertEquals(0, BufferPool.getPoolSize(TEST_BUFFER_SIZE));
+        assertEquals(currentPoolSize, BufferPool.getPoolSize(TEST_BUFFER_SIZE));
         ReusableBuffer viewBuffer = buf.createViewBuffer();
         // Create a view buffer of a view buffer.
         ReusableBuffer viewBuffer2 = viewBuffer.createViewBuffer();
