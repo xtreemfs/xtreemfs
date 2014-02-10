0dbb6681c68acc591f1642bca901eae6e5e65150
Michael Berlin <michael.berlin.xtreemfs@gmail.com>
2014-02-10 18:37:26 +0100

    foundation: ReusableBuffer: The fix in revision 4709359b2400 was incomplete and introduced three regressions.
    1st regression: position() and limit() were not independent of the parent buffer. For example, this broke the RAID0ObjectMapper code which uses view buffers when aligning chunks for write requests.
    
    2nd regression: position() did not start at 0 (while the size was correct and equaled length-offset). The RPC layer did not cope with this problem.
    
    These two problems were fixed by creating the view buffer using java.nio.ByteBuffer#slice().
    
    3rd regression: range(int, int), shrink(int) and enlarge(int) threw NullPointerException when the view buffer was no longer backed by a reusable buffer.
    
    Extended the code accordingly and wrote unit tests which cover all known cases.

--- java/foundation/test/org/xtreemfs/foundation/buffer/ReusableBufferTest.java
+++ java/foundation/test/org/xtreemfs/foundation/buffer/ReusableBufferTest.java
@@ -25,7 +25,7 @@ public class ReusableBufferTest {
 
         assertEquals("Yagga", result);
     }
-    
+
     @Test
     public final void testRecursiveNonResuableViewBuffer() {
         ReusableBuffer rb = ReusableBuffer.wrap("Yagga Yagga".getBytes());
@@ -36,5 +36,141 @@ public class ReusableBufferTest {
 
         ReusableBuffer viewBuffer2 = viewBuffer.createViewBuffer();
         assertFalse(viewBuffer2.isReusable());
+    }
+
+    @Test
+    public final void testNonResuableViewBufferHasIndependentPositions() {
+        ReusableBuffer rb = ReusableBuffer.wrap("Yagga Yagga".getBytes());
+        assertFalse(rb.isReusable());
+        rb.position(0);
+        rb.limit(5);
+
+        ReusableBuffer viewBuffer = rb.createViewBuffer();
+        assertFalse(viewBuffer.isReusable());
+        viewBuffer.position(1);
+        viewBuffer.limit(4);
+
+        assertEquals(0, rb.position());
+        assertEquals(5, rb.limit());
+        assertEquals(1, viewBuffer.position());
+        assertEquals(4, viewBuffer.limit());
+
+        ReusableBuffer viewBufferOfViewBuffer = viewBuffer.createViewBuffer();
+        assertFalse(viewBufferOfViewBuffer.isReusable());
+        viewBufferOfViewBuffer.position(2);
+        viewBufferOfViewBuffer.limit(3);
+
+        assertEquals(0, rb.position());
+        assertEquals(5, rb.limit());
+        assertEquals(1, viewBuffer.position());
+        assertEquals(4, viewBuffer.limit());
+        assertEquals(2, viewBufferOfViewBuffer.position());
+        assertEquals(3, viewBufferOfViewBuffer.limit());
+    }
+
+    @Test
+    public final void testRange() {
+        // Test with a view buffer and with a reusable buffer.
+        ReusableBuffer[] buffers = new ReusableBuffer[] { ReusableBuffer.wrap("Yagga Yagga".getBytes()),
+                BufferPool.allocate(12) };
+        for (ReusableBuffer buf : buffers) {
+            buf.range(1, 4);
+            assertEquals(0, buf.position());
+            assertEquals(4, buf.limit());
+            assertEquals(4, buf.capacity());
+            assertEquals(4, buf.remaining());
+
+            // Now create a view buffer which will have a different range.
+            ReusableBuffer viewBuf = buf.createViewBuffer();
+            viewBuf.range(2, 2);
+            assertEquals(0, viewBuf.position());
+            assertEquals(2, viewBuf.limit());
+            assertEquals(2, viewBuf.capacity());
+            assertEquals(2, viewBuf.remaining());
+
+            // The range of the original buffer was not affected.
+            assertEquals(0, buf.position());
+            assertEquals(4, buf.limit());
+            assertEquals(4, buf.capacity());
+            assertEquals(4, buf.remaining());
+
+            if (buf.isReusable()) {
+                BufferPool.free(buf);
+            }
+        }
+    }
+
+    @Test
+    public final void testShrink() {
+        // Test with a view buffer and with a reusable buffer.
+        ReusableBuffer[] buffers = new ReusableBuffer[] { ReusableBuffer.wrap("Yagga Yagga".getBytes()),
+                BufferPool.allocate(12) };
+        for (ReusableBuffer buf : buffers) {
+            buf.shrink(4);
+            assertEquals(0, buf.position());
+            assertEquals(4, buf.limit());
+            assertEquals(4, buf.capacity());
+            assertEquals(4, buf.remaining());
+
+            // Now create a view buffer which will have a different range.
+            ReusableBuffer viewBuf = buf.createViewBuffer();
+            viewBuf.shrink(2);
+            assertEquals(0, viewBuf.position());
+            assertEquals(2, viewBuf.limit());
+            assertEquals(2, viewBuf.capacity());
+            assertEquals(2, viewBuf.remaining());
+
+            // The range of the original buffer was not affected.
+            assertEquals(0, buf.position());
+            assertEquals(4, buf.limit());
+            assertEquals(4, buf.capacity());
+            assertEquals(4, buf.remaining());
+
+            if (buf.isReusable()) {
+                BufferPool.free(buf);
+            }
+        }
+    }
+
+    @Test
+    public final void testEnlarge() {
+        // Test with a view buffer and with a reusable buffer.
+        ReusableBuffer[] buffers = new ReusableBuffer[] { ReusableBuffer.wrap("Yagga Yagga".getBytes()),
+                BufferPool.allocate(11) };
+        for (ReusableBuffer buf : buffers) {
+            int originalBufSize = buf.capacity();
+
+            // When there is no underlying, larger buffer, enlarge won't have an effect (the capacity of the
+            // underlying buffer can be higher than the current buffer size.)
+            assertFalse(buf.enlarge(buf.capacityUnderlying() + 1));
+            // Enlarging to the current size should always work.
+            assertTrue(buf.enlarge(originalBufSize));
+            assertEquals(originalBufSize, buf.capacity());
+
+            // Create a view buffer first which is smaller than the original one.
+            buf.limit(originalBufSize / 2);
+            ReusableBuffer smallerBuf = buf.createViewBuffer();
+
+            // Enlarge it, but not larger than the underlying buffer.
+            int newBufSize = smallerBuf.capacity() * 2;
+            assertTrue(smallerBuf.enlarge(newBufSize));
+            assertEquals(0, buf.position());
+            assertEquals(newBufSize, smallerBuf.limit());
+            assertEquals(newBufSize, smallerBuf.capacity());
+            assertEquals(newBufSize, smallerBuf.remaining());
+
+            // You cannot enlarge it over the capacity of the original buffer.
+            assertFalse(smallerBuf.enlarge(buf.capacityUnderlying() + 1));
+
+            // The range of the original buffer was not affected.
+            assertEquals(0, buf.position());
+            assertEquals(originalBufSize / 2, buf.limit());
+            assertEquals(originalBufSize, buf.capacity());
+            assertEquals(originalBufSize / 2, buf.remaining());
+
+            if (buf.isReusable()) {
+                BufferPool.free(buf);
+            }
+        }
     }
 }
