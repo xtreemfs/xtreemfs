aea2e0950850ac1db753d1cd679e0520f17215cc
Thorsten Schuett <schuett@zib.de>
2014-02-26 17:15:02 +0100

    disable http-servers for buildroot

--- packaging/buildroot/xtreemfs.mk
+++ packaging/buildroot/xtreemfs.mk
@@ -1,8 +1,8 @@
-#################
+#################################################
 #
 # XtreemFS package for client and server targets.
 #
-#################
+#################################################
 
 XTREEMFS_VERSION=master
 XTREEMFS_SITE="https://code.google.com/p/xtreemfs/"
@@ -13,12 +13,10 @@ define XTREEMFS_USERS
 endef
 
 define XTREEMFS_BUILD_CMDS
-	XTREEMFS_CLIENT_BUILD_DIR=cpp/build
 # google-protobuf always tries to run its unit tests with a given 'protoc'. Make sure that these tests always pass by setting the 'protoc' path to '/bin/true'.
-	(cd $(@D)/cpp/thirdparty/protobuf-2.5.0 && $(TARGET_MAKE_ENV) CC="$(TARGET_CC)" CXX="$(TARGET_CXX)" LD="$(TARGET_LD) -L$(STAGING_DIR)/lib -L$(STAGING_DIR)/usr/lib" ./configure --host=x86_64-unknown-linux-gnu --target=i686-unknown-linux-gnu --with-protoc=/bin/true)
+	(cd $(@D)/cpp/thirdparty/protobuf-2.5.0 && $(TARGET_MAKE_ENV) CC="$(TARGET_CC)" CXX="$(TARGET_CXX)" LD="$(TARGET_LD) -L$(STAGING_DIR)/lib -L$(STAGING_DIR)/usr/lib" ./configure --host=$(BR2_ARCH)"-unknown-linux-gnu" --with-protoc=/bin/true)
 	$(TARGET_MAKE_ENV) make -C $(@D)/cpp/thirdparty/protobuf-2.5.0
-	(cd $(@D) && CC="$(TARGET_CC)" CXX="$(TARGET_CXX)" LD="$(TARGET_LD) -L$(STAGING_DIR)/lib -L$(STAGING_DIR)/usr/lib" cmake -Hcpp -B$(@D)/cpp/build --check-build-system CMakeFiles/Makefile.cmake 0  -DBOOST_ROOT=$(STAGING_DIR)/usr) -DSKIP_FUSE=true
-	echo $(@D)
+	(cd $(@D) && CC="$(TARGET_CC)" CXX="$(TARGET_CXX)" LD="$(TARGET_LD) -L$(STAGING_DIR)/lib -L$(STAGING_DIR)/usr/lib" cmake -DSKIP_FUSE=true -Hcpp -B$(@D)/cpp/build --check-build-system CMakeFiles/Makefile.cmake 0  -DBOOST_ROOT=$(STAGING_DIR)/usr)
 	$(TARGET_MAKE_ENV) make -C $(@D)/cpp/build
 	cp -p $(@D)/cpp/build/*.xtreemfs $(@D)/bin
 	cp -p $(@D)/cpp/build/xtfsutil $(@D)/bin
@@ -26,6 +24,9 @@ define XTREEMFS_BUILD_CMDS
 endef
 
 define XTREEMFS_INSTALL_TARGET_CMDS
+	sed -i 's/http_port = 30640/http_port = -1/'g $(@D)/etc/xos/xtreemfs/osdconfig.properties
+	sed -i 's/http_port = 30638/http_port = -1/'g $(@D)/etc/xos/xtreemfs/dirconfig.properties
+	sed -i 's/http_port = 30636/http_port = -1/'g $(@D)/etc/xos/xtreemfs/mrcconfig.properties
 	$(TARGET_MAKE_ENV) $(MAKE) DESTDIR=$(TARGET_DIR) -C $(@D) install-client install-server
 	$(@D)/etc/init.d/generate_initd_scripts.sh
 	sed 's/java -ea /jamvm /'g $(@D)/etc/init.d/xtreemfs-dir > $(TARGET_DIR)/etc/init.d/xtreemfs-dir
