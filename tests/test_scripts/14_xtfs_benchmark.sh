898e0a3618f9a599d153a7329d3fe6d6b30edbe6
jensvfischer <jensvfischer@gmail.com>
2014-03-29 20:50:15 +0100

    jens-benchmark/server: Refactor xtfs_benchmark in xtestenv
    Implement as test on the predefined volumes, not the as system test.
    
    Conflicts:
    	tests/test_config.py

--- tests/test_scripts/14_xtfs_benchmark.sh
+++ tests/test_scripts/14_xtfs_benchmark.sh
@@ -6,18 +6,12 @@ XTREEMFS=$1
 DIR_SERVER=$2
 MRC_SERVER=$3
 TEST_DIR=$4
-
-LOGFILE=$TEST_DIR/log/xtfs_benchmark.log
-
-# save stdout and stderr to file descriptors 3 and 4, then redirect them
-exec 3>&1 4>&2 >> $LOGFILE 2>&1
+VOLUME="$(basename $(dirname $(pwd)))"
 
 exec $JAVA_HOME/bin/java -ea -cp $XTREEMFS/java/servers/dist/XtreemFS.jar:$XTREEMFS/java/foundation/dist/Foundation.jar:$XTREEMFS/java/lib/*:/usr/share/java/XtreemFS.jar:/usr/share/java/protobuf-java-2.3.0.jar:/usr/share/java/Foundation.jar:. \
-  org.xtreemfs.utils.xtfs_benchmark.xtfs_benchmark -sw -sr -rw -rr -fw -fr -t 2 --stripe-size 128K --stripe-width 1 -ssize 1g -rsize 100m --file-size 4K --basefile-size 3g --dir-address $DIR_SERVER volumeA volumeB
+  org.xtreemfs.utils.xtfs_benchmark.xtfs_benchmark -sw -sr -rw -rr -fw -fr --stripe-size 128K --stripe-width 1 -ssize 1g -rsize 100m --file-size 4K --basefile-size 3g --dir-address $DIR_SERVER $VOLUME
 
 # minimal version for testing
 # exec $JAVA_HOME/bin/java -ea -cp $XTREEMFS/java/servers/dist/XtreemFS.jar:$XTREEMFS/java/foundation/dist/Foundation.jar:$XTREEMFS/java/lib/*:/usr/share/java/XtreemFS.jar:/usr/share/java/protobuf-java-2.3.0.jar:/usr/share/java/Foundation.jar:. \
-#   org.xtreemfs.utils.xtfs_benchmark.xtfs_benchmark -sw -sr -rw -rr -fw -fr -t 2 --stripe-size 128K --stripe-width 1 -ssize 10m -rsize 1m --file-size 4K --basefile-size 100m --dir-address $DIR_SERVER volumeA volumeB
+#   org.xtreemfs.utils.xtfs_benchmark.xtfs_benchmark -sw -sr -rw -rr -fw -fr -t 1 --stripe-size 128K --stripe-width 1 -ssize 10m -rsize 1m --file-size 4K --basefile-size 100m --dir-address $DIR_SERVER $VOLUME
 
-# restore stdout and stderr
-exec 1>&3 2>&4
