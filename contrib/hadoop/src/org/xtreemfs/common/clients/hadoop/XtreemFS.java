5bc04dd4176b11ef06d572a53f4c47b8be279eb7
Lukas Kairies <lukas.xtreemfs@gmail.com>
2014-04-09 14:00:55 +0200

    lkairies-hadoop2_integration/contrib, docs: Re-named XtfsFileSystem to XtreemFS and updated xtreemfs user guide.

--- contrib/hadoop/src/org/xtreemfs/common/clients/hadoop/XtreemFS.java
+++ contrib/hadoop/src/org/xtreemfs/common/clients/hadoop/XtreemFS.java
@@ -8,15 +8,15 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.DelegateToFileSystem;
 import org.apache.hadoop.fs.FileSystem;
 
-public class XtfsFileSystem extends DelegateToFileSystem {
+public class XtreemFS extends DelegateToFileSystem {
 
-	protected XtfsFileSystem(URI theUri, FileSystem theFsImpl,
+	protected XtreemFS(URI theUri, FileSystem theFsImpl,
 			Configuration conf, String supportedScheme,
 			boolean authorityRequired) throws IOException, URISyntaxException {
 		super(theUri, theFsImpl, conf, supportedScheme, authorityRequired);
 	}
 
-	protected XtfsFileSystem(URI theUri, Configuration conf) throws IOException, URISyntaxException {
+	protected XtreemFS(URI theUri, Configuration conf) throws IOException, URISyntaxException {
 		this(theUri, new XtreemFSFileSystem(), conf, theUri.getScheme(), false);		
 	}
 }
