c5e49ad99648c8e6e3e73df5b5565ccebe2b6c90
Lukas Kairies <lukas.xtreemfs@gmail.com>
2014-04-29 13:29:32 +0200

    lkairies-scheduler-web-interface/interface: Added scheduler to Service Type.

--- cpp/generated/xtreemfs/DIR.pb.h
+++ cpp/generated/xtreemfs/DIR.pb.h
@@ -63,11 +63,12 @@ enum ServiceType {
   SERVICE_TYPE_MRC = 1,
   SERVICE_TYPE_OSD = 2,
   SERVICE_TYPE_VOLUME = 3,
-  SERVICE_TYPE_DIR = 4
+  SERVICE_TYPE_DIR = 4,
+  SERVICE_TYPE_SCHEDULER = 5
 };
 bool ServiceType_IsValid(int value);
 const ServiceType ServiceType_MIN = SERVICE_TYPE_MIXED;
-const ServiceType ServiceType_MAX = SERVICE_TYPE_DIR;
+const ServiceType ServiceType_MAX = SERVICE_TYPE_SCHEDULER;
 const int ServiceType_ARRAYSIZE = ServiceType_MAX + 1;
 
 const ::google::protobuf::EnumDescriptor* ServiceType_descriptor();
