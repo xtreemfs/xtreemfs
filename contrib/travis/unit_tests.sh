bc3ea1718edb55aaeef6c6b31a7029f0312af25c
Christoph Kleineweber <kleineweber@zib.de>
2014-06-06 11:30:34 +0100

    tests: removed C++ unittests (temporarily) from Travis-CI

--- contrib/travis/unit_tests.sh
+++ contrib/travis/unit_tests.sh
@@ -7,11 +7,6 @@ junit_tests(){
   # return 0
 }
 
-cpp_unit_tests(){
-  tests/test_scripts/cpp_unit_tests.sh
-  # return 0
-}
-
 junit_tests
 jstatus="$?"
 if [ $jstatus -ne 0 ]; then
@@ -20,17 +15,9 @@ else
   export JUNIT_FAILED="false"
 fi
 
-cpp_unit_tests
-cstatus="$?"
-if [ $cstatus -ne 0 ]; then
-  export CPP_FAILED="true"
-else
-  export CPP_FAILED="false"
-fi
-
 cd $XTREEMFS_DIR
 
-if [[ $jstatus -eq 0 ]] && [[ $cstatus -eq 0 ]]; then
+if [[ $jstatus -eq 0 ]]; then
   return 0
 else
   return 1
