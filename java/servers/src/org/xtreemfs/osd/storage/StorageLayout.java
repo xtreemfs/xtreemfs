97a51f2b9c6284fe5810f52c0d1ea413453d2f60
Christoph Kleineweber <kleineweber@zib.de>
2014-11-03 17:56:09 +0100

    servers: updated javadoc in StorageLayout

--- java/servers/src/org/xtreemfs/osd/storage/StorageLayout.java
+++ java/servers/src/org/xtreemfs/osd/storage/StorageLayout.java
@@ -8,6 +8,14 @@
 
 package org.xtreemfs.osd.storage;
 
+import org.xtreemfs.common.xloc.StripingPolicyImpl;
+import org.xtreemfs.foundation.buffer.BufferPool;
+import org.xtreemfs.foundation.buffer.ReusableBuffer;
+import org.xtreemfs.osd.InternalObjectData;
+import org.xtreemfs.osd.OSDConfig;
+import org.xtreemfs.osd.replication.ObjectSet;
+import org.xtreemfs.pbrpc.generatedinterfaces.OSD.TruncateLog;
+
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -16,15 +24,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Stack;
-
-import org.xtreemfs.common.xloc.StripingPolicyImpl;
-import org.xtreemfs.foundation.buffer.BufferPool;
-import org.xtreemfs.foundation.buffer.ReusableBuffer;
-import org.xtreemfs.osd.InternalObjectData;
-import org.xtreemfs.osd.OSDConfig;
-import org.xtreemfs.osd.replication.ObjectSet;
-import org.xtreemfs.pbrpc.generatedinterfaces.OSD.ObjectData;
-import org.xtreemfs.pbrpc.generatedinterfaces.OSD.TruncateLog;
 
 /**
  * Abstracts object data access from underlying on-disk storage layout.
@@ -176,20 +175,19 @@ public abstract class StorageLayout {
      * 
      * @param fileId
      *            fileId of the object
+     * @param md
+     *            file metadata
      * @param objNo
      *            object number
+     * @param offset
+     *            offset
+     * @param length
+     *            length
      * @param version
      *            version to be read
-     * @param checksum
-     *            the checksum currently stored with the object
-     * @param sp
-     *            the striping policy assigned to the file
-     * @param osdNumber
-     *            the number of the OSD assigned to the object
      * @throws java.io.IOException
      *             when the object cannot be read
-     * @return a buffer containing the object, or a <code>null</code> if the
-     *         object does not exist
+     * @return ObjectInformation
      */
     
     public abstract ObjectInformation readObject(String fileId, FileMetadata md, long objNo, int offset,
@@ -200,21 +198,20 @@ public abstract class StorageLayout {
      * 
      * @param fileId
      *            the file Id the object belongs to
-     * @param objNo
-     *            object number
+     * @param md
+     *            file metadata
      * @param data
      *            buffer with the data to be written
-     * @param version
-     *            the version to be written
+     * @param objNo
+     *            object number
      * @param offset
      *            the relative offset in the object at which to write the buffer
-     * @param currentChecksum
-     *            the checksum currently assigned to the object; if OSD
-     *            checksums are disabled, <code>null</code> can be used
-     * @param sp
-     *            the striping policy assigned to the file
-     * @param osdNumber
-     *            the number of the OSD responsible for the object
+     * @param newVersion
+     *            new file version
+     * @param sync
+     *            write synchronously
+     * @param cow
+     *            use cow
      * @throws java.io.IOException
      *             when the object cannot be written
      */
@@ -228,6 +225,7 @@ public abstract class StorageLayout {
      * @param md
      * @param objNo
      * @param newLength
+     * @param newVersion
      * @param cow
      * @throws IOException
      */
@@ -239,6 +237,8 @@ public abstract class StorageLayout {
      * 
      * @param fileId
      *            the ID of the file
+     * @param deleteMetadata
+     *            delete file metadata
      * @throws IOException
      *             if an error occurred while deleting the objects
      */
@@ -249,6 +249,8 @@ public abstract class StorageLayout {
      * 
      * @param fileId
      *            the ID of the file
+     * @param md
+     *            file metadata
      * @param objNo
      *            the number of the object to delete
      * @param version
@@ -264,10 +266,10 @@ public abstract class StorageLayout {
      * 
      * @param fileId
      *            the ID of the file
+     * @param md
+     *            file metadata
      * @param objNo
      *            the number of the object to create
-     * @param sp
-     *            the striping policy assigned to the file
      * @param version
      *            the version of the object to create
      * @param size
@@ -416,6 +418,7 @@ public abstract class StorageLayout {
      * returns a list of all local saved objects of this file
      * 
      * @param fileId
+     * @param md
      * @return null, if file does not exist, otherwise objectList
      */
     public abstract ObjectSet getObjectSet(String fileId, FileMetadata md);
