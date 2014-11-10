49e2ac2dd85d5ca94441620a8079760822843cef
Robert Schmidtke <ro.schmidtke@gmail.com>
2014-11-10 14:43:36 +0100

    cpp: libxtreemfs: added options test for command line parsing

--- cpp/test/libxtreemfs/options_test.cpp
+++ cpp/test/libxtreemfs/options_test.cpp
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2014 by Robert Schmidtke, Zuse Institute Berlin
+ *
+ * Licensed under the BSD License, see LICENSE file for details.
+ *
+ */
+
 #include <gtest/gtest.h>
 
 #include "libxtreemfs/options.h"
@@ -5,12 +12,75 @@
 namespace xtreemfs {
 
 TEST(OptionsTest, TestParseCommandLineAndConfigFile) {
-  const char* n_argv[] = { "executable", "--log-level=client.log" };
-
   xtreemfs::Options options;
-  options.ParseCommandLine(2, const_cast<char**>(n_argv));
-
-  ASSERT_EQ("INFO", options.log_file_path);
+  
+  {
+    // Fail on multiple log level specifications.
+    const char* n_argv[] = { "executable", "--log-level=DEBUG", "-dINFO" };
+    ASSERT_THROW(
+        options.ParseCommandLine(3, const_cast<char**>(n_argv)),
+        std::exception
+    );
+  }
+  
+  {
+    // Explicit specification overrides alternative specification.
+    // Short alternative specification.
+    const char* n_argv[] = {
+        "executable",
+        "--log-level=DEBUG",
+        "-o", "-d=INFO"
+    };
+    options.ParseCommandLine(4, const_cast<char**>(n_argv));
+    ASSERT_EQ("DEBUG", options.log_level_string);
+  }
+  
+  {
+    // Alternative specification complements explicit specifications.
+    const char* n_argv[] = {
+        "executable",
+        "--log-file-path=/tmp/test.log",
+        "-o", "-d=INFO"
+    };
+    options.ParseCommandLine(4, const_cast<char**>(n_argv));
+    ASSERT_EQ("/tmp/test.log", options.log_file_path);
+    ASSERT_EQ("INFO", options.log_level_string);
+  }
+  
+  {
+    // Multiple long alternative specifications.
+    const char* n_argv[] = {
+        "executable",
+        "-o", "log-file-path=/tmp/test.log",
+        "-o", "log-level=INFO"
+    };
+    options.ParseCommandLine(5, const_cast<char**>(n_argv));
+    ASSERT_EQ("/tmp/test.log", options.log_file_path);
+    ASSERT_EQ("INFO", options.log_level_string);
+  }
+  
+  {
+    // Comma separated alternative specification.
+    const char* n_argv[] = {
+        "executable",
+        "-o", "log-file-path=/tmp/test.log,-d=INFO"
+    };
+    options.ParseCommandLine(3, const_cast<char**>(n_argv));
+    ASSERT_EQ("/tmp/test.log", options.log_file_path);
+    ASSERT_EQ("INFO", options.log_level_string);
+  }
+  
+  {
+    // Example from what can be passed in from /etc/fstab.
+    const char* n_argv[] = {
+        "executable",
+        "-o", "_netdev,pkcs12-file-path=/tmp/pkcs12.p12,log-level=DEBUG"
+    };
+    options.ParseCommandLine(3, const_cast<char**>(n_argv));
+    ASSERT_EQ("/tmp/pkcs12.p12", options.ssl_pkcs12_path);
+    ASSERT_EQ("DEBUG", options.log_level_string);
+  }
+  
 }
 
 }  // namespace xtreemfs
