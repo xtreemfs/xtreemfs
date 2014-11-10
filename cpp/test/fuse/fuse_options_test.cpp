759270b60455f4c90b1d2ba5dd56b4805fff24a7
Robert Schmidtke <ro.schmidtke@gmail.com>
2014-11-10 16:00:49 +0100

    cpp: fuse: extended fuse options test to account for now different handling of -o option

--- cpp/test/fuse/fuse_options_test.cpp
+++ cpp/test/fuse/fuse_options_test.cpp
@@ -32,13 +32,13 @@ class FuseOptionsTest : public ::testing::Test {
   }
 };
 
-TEST_F(FuseOptionsTest, TestCommandLineMultipleOptionsPerMinusoOption) {
+TEST_F(FuseOptionsTest, TestCommandLineMultipleOptionsPerFuseOptionAndMinusoOption) {
   int argc = 7;
   char** argv = new char*[argc];
   argv[0] = strdup("mount.xtreemfs");
-  argv[1] = strdup("-o");
+  argv[1] = strdup("--fuse_option");
   argv[2] = strdup("allow_other,bogus");
-  argv[3] = strdup("-o");
+  argv[3] = strdup("--fuse_option");
   argv[4] = strdup("bogus2");
   argv[5] = strdup("localhost/test");
   argv[6] = strdup("/mnt/xtreemfs");
@@ -56,6 +56,24 @@ TEST_F(FuseOptionsTest, TestCommandLineMultipleOptionsPerMinusoOption) {
   // Split options are added to the end of fuse_options.
   EXPECT_EQ("allow_other", options.fuse_options[1]);
   EXPECT_EQ("bogus", options.fuse_options[2]);
+  
+  // when using -o, split options are ordered because they are processed in
+  // Options::ParseCommandLine.
+  free(argv[1]);
+  argv[1] = strdup("-o");
+  free(argv[3]);
+  argv[1] = strdup("-o");
+  
+  ASSERT_NO_THROW({
+    options.ParseCommandLine(argc, argv);
+  });
+  ASSERT_TRUE(!options.show_help && !options.empty_arguments_list
+              && !options.show_version);
+
+  ASSERT_EQ(3, options.fuse_options.size());
+  EXPECT_EQ("allow_other", options.fuse_options[0]);
+  EXPECT_EQ("bogus", options.fuse_options[1]);
+  EXPECT_EQ("bogus2", options.fuse_options[2]);
 
   for (int i = 0; i < argc; i++) {
     free(argv[i]);
