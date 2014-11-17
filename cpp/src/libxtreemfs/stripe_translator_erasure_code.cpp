092d8f2dfc4b8a5dcf08af2b8834d022af743122
Jan Fajerski <fajerski@zib.de>
2014-11-17 14:27:55 +0100

    janf-erasurecodes/cpp: stripe_translator::TranslateReadRequest orders ReadOps in ideal read sequence
    For a simple 2/1 striping this means putting the parity read at the end, since ideally only the data
    stripes will be read.

--- cpp/src/libxtreemfs/stripe_translator_erasure_code.cpp
+++ cpp/src/libxtreemfs/stripe_translator_erasure_code.cpp
@@ -8,7 +8,6 @@
 
 #include <algorithm>
 #include <vector>
-#include <boost/scoped_array.hpp>
 
 using namespace std;
 using namespace xtreemfs::pbrpc;
@@ -30,18 +29,19 @@ void StripeTranslatorErasureCode::TranslateWriteRequest(
   char *buf_redundance = NULL;
 
   size_t total_size = (size / (width - parity_width)) * width;
+  // object number for stripe (parity stripes are not counted)
   size_t obj_number = 0;
+  // actual object number to calculate the osd number
+  size_t real_obj_number = 0;
 
   size_t start = 0;
   size_t written_blocks = 0;
 
   while (written_blocks < total_size) {
 
-    bool parity_block = false;
-
     std::vector<size_t> osd_offsets;
 
-    size_t osd_number = obj_number % (width);
+    size_t osd_number = real_obj_number % width;
     osd_offsets.push_back(osd_number);
 
     //is it a parity chunk?
@@ -54,23 +54,22 @@ void StripeTranslatorErasureCode::TranslateWriteRequest(
             ^ buf[start - ((j-1) * stripe_size) + i];
       }
 
+      /* use the last object number used for a data stripe
+       * this caters towards the calculation of a correct  filesize
+       * ...and hopefully nothing else */
       operations->push_back(
-          WriteOperation(obj_number, osd_offsets, stripe_size, 0,
+          WriteOperation(obj_number - 1, osd_offsets, stripe_size, 0,
               buf_redundance, true));
-
-      parity_block = true;
-
-    }
-
-    if (!parity_block) {
+    } else {
       operations->push_back(
           WriteOperation(obj_number, osd_offsets, stripe_size, 0,
               buf + start));
 
       start += stripe_size;
+      obj_number++;
     }
+    real_obj_number++;
     written_blocks += stripe_size;
-    obj_number++;
   }
 }
 
@@ -80,6 +79,7 @@ void StripeTranslatorErasureCode::TranslateReadRequest(
     int64_t offset,
     PolicyContainer policies,
     std::vector<ReadOperation>* operations) const {
+  std::vector<ReadOperation>::iterator par_reads = operations->end();
   // stripe size is stored in kB
   unsigned int stripe_size = (*policies.begin())->stripe_size() * 1024;
   // number of OSD to distribute parity chunks
@@ -90,39 +90,37 @@ void StripeTranslatorErasureCode::TranslateReadRequest(
 
   size_t total_size = (size / (width - parity_width)) * width;
   size_t obj_number = 0;
+  size_t real_obj_number = 0;
 
   size_t start = 0;
   size_t read_blocks = 0;
 
   while (read_blocks < total_size) {
 
-    bool parity_block = false;
-
     std::vector<size_t> osd_offsets;
 
-    size_t osd_number = obj_number % (width);
+    size_t osd_number = real_obj_number % (width);
     osd_offsets.push_back(osd_number);
 
     //is it a parity chunk?
     if (osd_number >= width - parity_width) {
       buf_redundance = new char[stripe_size];
-      // operations->push_back(
-      //     ReadOperation(obj_number, osd_offsets, stripe_size, 0,
-      //         buf_redundance));
-
-      parity_block = true;
-
-    }
-
-    if (!parity_block) {
+      /* use the last object number used for a data stripe
+       * this caters towards the calculation of a correct  filesize
+       * ...and hopefully nothing else */
       operations->push_back(
+          ReadOperation(obj_number - 1, osd_offsets, stripe_size, 0,
+              buf_redundance));
+    } else {
+      par_reads = operations->insert(par_reads,
           ReadOperation(obj_number, osd_offsets, stripe_size, 0,
               buf + start));
-
+      par_reads++;
       start += stripe_size;
+      obj_number++;
     }
     read_blocks += stripe_size;
-    obj_number++;
+    real_obj_number++;
   }
 }
 
