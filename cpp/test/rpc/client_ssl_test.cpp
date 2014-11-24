6236afba4f479fd62594235bc67bf33aa979c299
Robert Schmidtke <ro.schmidtke@gmail.com>
2014-11-24 18:17:16 +0100

    cpp: libxtreemfs: added two initial SSL tests

--- cpp/test/rpc/client_ssl_test.cpp
+++ cpp/test/rpc/client_ssl_test.cpp
@@ -29,13 +29,11 @@ namespace rpc {
 class ExternalService {
 public:
   ExternalService(
-      std::string config_file_name,
-      std::string xtreemfs_dir)
-      : config_file_name_(config_file_name),
-        xtreemfs_dir_(xtreemfs_dir) {
-        
-    java_home_ = strdup(getenv("JAVA_HOME"));
-    if (java_home_.empty()) {
+      std::string config_file_name)
+      : config_file_name_(config_file_name) {
+    
+    char *java_home = getenv("JAVA_HOME");
+    if (java_home == NULL || (java_home_ = strdup(java_home)).empty()) {
       if (Logging::log->loggingActive(LEVEL_WARN)) {
         Logging::log->getLog(LEVEL_WARN) << "JAVA_HOME is empty."
             << std::endl;
@@ -46,17 +44,10 @@ public:
       java_home_ += "/";
     }
     
-    if (xtreemfs_dir_.empty()) {
-      if (Logging::log->loggingActive(LEVEL_WARN)) {
-        Logging::log->getLog(LEVEL_WARN) << "XTREEMFS_DIR is empty."
-            << std::endl;
-      }
-    }
-    
-    classpath_ = xtreemfs_dir_ + "java/servers/dist/XtreemFS.jar";
-    classpath_ += ":" + xtreemfs_dir_ + "java/foundation/dist/Foundation.jar";
-    classpath_ += ":" + xtreemfs_dir_ + "java/flease/dist/Flease.jar";
-    classpath_ += ":" + xtreemfs_dir_ + "java/lib/*";
+    classpath_ = "java/servers/dist/XtreemFS.jar";
+    classpath_ += ":java/foundation/dist/Foundation.jar";
+    classpath_ += ":java/flease/dist/Flease.jar";
+    classpath_ += ":java/lib/*";
     
     service_pid_ = -1;
   }
@@ -95,15 +86,14 @@ private:
   std::string config_file_name_;
   std::string java_home_;
   std::string classpath_;
-  std::string xtreemfs_dir_;
   
   pid_t service_pid_;
 };
   
 class ExternalDIR : public ExternalService {
 public:
-  ExternalDIR(std::string config_file_name, std::string xtreemfs_dir)
-  : ExternalService(config_file_name, xtreemfs_dir) {}
+  ExternalDIR(std::string config_file_name)
+  : ExternalService(config_file_name) {}
   
   void Start() {
     ExternalService::Start("org.xtreemfs.dir.DIR");
@@ -112,8 +102,8 @@ public:
 
 class ExternalMRC : public ExternalService {
 public:
-  ExternalMRC(std::string config_file_name, std::string xtreemfs_dir)
-  : ExternalService(config_file_name, xtreemfs_dir) {}
+  ExternalMRC(std::string config_file_name)
+  : ExternalService(config_file_name) {}
   
   void Start() {
     ExternalService::Start("org.xtreemfs.mrc.MRC");
@@ -122,8 +112,8 @@ public:
 
 class ExternalOSD : public ExternalService {
 public:
-  ExternalOSD(std::string config_file_name, std::string xtreemfs_dir)
-  : ExternalService(config_file_name, xtreemfs_dir) {}
+  ExternalOSD(std::string config_file_name)
+  : ExternalService(config_file_name) {}
   
   void Start() {
     ExternalService::Start("org.xtreemfs.osd.OSD");
@@ -131,27 +121,26 @@ public:
 };
 
 class ClientTest : public ::testing::Test {
-public:
-  ClientTest() {
-    xtreemfs_dir_ = strdup(getenv("XTREEMFS_DIR"));
-  }
-  
- protected:
+protected:
   virtual void SetUp() {
     initialize_logger(LEVEL_WARN);
     
-    external_dir_.reset(new ExternalDIR(dir_config_file_, xtreemfs_dir_));
+    external_dir_.reset(new ExternalDIR(dir_config_file_));
     external_dir_->Start();
-    external_mrc_.reset(new ExternalMRC(mrc_config_file_, xtreemfs_dir_));
+    external_mrc_.reset(new ExternalMRC(mrc_config_file_));
     external_mrc_->Start();
-    external_osd_.reset(new ExternalOSD(osd_config_file_, xtreemfs_dir_));
+    external_osd_.reset(new ExternalOSD(osd_config_file_));
     external_osd_->Start();
     
     auth_.set_auth_type(AUTH_NONE);
     user_credentials_.set_username("client_ssl_test");
     user_credentials_.add_groups("client_ssl_test");
     
-    client_.reset(new ClientImplementation(dir_url_,
+    options_.retry_delay_s = 5;
+    
+    mrc_url_.ParseURL(kMRC);
+    dir_url_.ParseURL(kDIR);
+    client_.reset(new ClientImplementation(dir_url_.service_addresses,
                                            user_credentials_,
                                            options_.GenerateSSLOptions(),
                                            options_));
@@ -167,7 +156,20 @@ public:
     external_dir_->Shutdown();
   }
   
-protected:
+  void CreateOpenDeleteVolume(std::string volume_name) {
+    client_->CreateVolume(mrc_url_.service_addresses,
+                        auth_,
+                        user_credentials_,
+                        volume_name);
+    client_->OpenVolume(volume_name,
+                        options_.GenerateSSLOptions(),
+                        options_);
+    client_->DeleteVolume(mrc_url_.service_addresses,
+                          auth_,
+                          user_credentials_,
+                          volume_name);
+  }
+  
   boost::scoped_ptr<ExternalDIR> external_dir_;
   boost::scoped_ptr<ExternalMRC> external_mrc_;
   boost::scoped_ptr<ExternalOSD> external_osd_;
@@ -175,7 +177,8 @@ protected:
   boost::scoped_ptr<xtreemfs::Client> client_;
   xtreemfs::Options options_;
   
-  std::string dir_url_;
+  xtreemfs::Options dir_url_;
+  xtreemfs::Options mrc_url_;
   
   std::string dir_config_file_;
   std::string mrc_config_file_;
@@ -183,29 +186,44 @@ protected:
 
   xtreemfs::pbrpc::Auth auth_;
   xtreemfs::pbrpc::UserCredentials user_credentials_;
-  
-  std::string xtreemfs_dir_;
 };
 
 class ClientNoSSLTest : public ClientTest {
 protected:
   virtual void SetUp() {
-    dir_config_file_ = xtreemfs_dir_ + "etc/xos/xtreemfs/dirconfig.test";
-    mrc_config_file_ = xtreemfs_dir_ + "etc/xos/xtreemfs/mrcconfig.test";
-    osd_config_file_ = xtreemfs_dir_ + "etc/xos/xtreemfs/osdconfig.test";
+    dir_config_file_ = "tests/configs/dirconfig_nossl.test";
+    mrc_config_file_ = "tests/configs/mrcconfig_nossl.test";
+    osd_config_file_ = "tests/configs/osdconfig_nossl.test";
+        
+    dir_url_.xtreemfs_url = "pbrpc://localhost:42638/";
+    mrc_url_.xtreemfs_url = "pbrpc://localhost:42636/";
     
-    options_.log_level_string = "DEBUG";
-    options_.log_file_path = "/tmp/xtreemfs_client_ssl_test_no_ssl";
+    ClientTest::SetUp();
+  }
+};
+
+class ClientSSLTest : public ClientTest {
+protected:
+  virtual void SetUp() {
+    dir_config_file_ = "tests/configs/dirconfig_ssl.test";
+    mrc_config_file_ = "tests/configs/mrcconfig_ssl.test";
+    osd_config_file_ = "tests/configs/osdconfig_ssl.test";
+        
+    dir_url_.xtreemfs_url = "pbrpcs://localhost:42638/";
+    mrc_url_.xtreemfs_url = "pbrpcs://localhost:42636/";
     
-    dir_url_ = "pbrpc://localhost:32638";
+    options_.ssl_pkcs12_path = "tests/certs/client_ssl_test/Client_Leaf_Chain.p12";
     
     ClientTest::SetUp();
   }
 };
 
 TEST_F(ClientNoSSLTest, TestNoSSL) {
-  // for now e.g. JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64 XTREEMFS_DIR=/home/robert/workspace/xtreemfs/ cpp/build/test_client_ssl
-  sleep(5);
+  CreateOpenDeleteVolume("test_no_ssl");
+}
+
+TEST_F(ClientSSLTest, TestNoSSL) {
+  CreateOpenDeleteVolume("test_ssl");
 }
 
 }  // namespace rpc
