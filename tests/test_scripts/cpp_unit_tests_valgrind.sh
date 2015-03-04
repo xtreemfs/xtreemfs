455d8d577eae14aeaa9e0da8671aee11b5ad48f8
Robert Schmidtke <ro.schmidtke@gmail.com>
2015-03-04 16:33:19 +0100

    cpp: valgrind: generating suppressions

--- tests/test_scripts/cpp_unit_tests_valgrind.sh
+++ tests/test_scripts/cpp_unit_tests_valgrind.sh
@@ -77,7 +77,7 @@ global_rc=0
 for test in test_*
 do
   set +e
-  valgrind --leak-check=full --show-reachable=yes --error-exitcode=23 --suppressions="${XTREEMFS_DIR}/cpp/valgrind.supp" ./$test &>>$VALGRIND_LOG_FILE
+  valgrind --leak-check=full --show-reachable=yes --error-exitcode=23 --gen-suppressions=yes --suppressions="${XTREEMFS_DIR}/cpp/valgrind.supp" ./$test &>>$VALGRIND_LOG_FILE
   rc=$?
   set -e
   # Add some whitespace to the logfile between runs.
