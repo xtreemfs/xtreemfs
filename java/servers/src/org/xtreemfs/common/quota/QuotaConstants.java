6f6b5838a9ddafeaf575cb77d98b90caf19313af
Robert Bärhold <baerhold@zib.de>
2015-09-23 15:54:33 +0200

    Common\quota: Changed unlimited voucher value to zero

--- java/servers/src/org/xtreemfs/common/quota/QuotaConstants.java
+++ java/servers/src/org/xtreemfs/common/quota/QuotaConstants.java
@@ -8,7 +8,7 @@ package org.xtreemfs.common.quota;
 
 public class QuotaConstants {
 
-    public final static long unlimitedVoucher = -1;
+    public final static long unlimitedVoucher = 0;
     public final static long unlimitedQuota   = 0;
     public final static long noQuota          = -1;
 
