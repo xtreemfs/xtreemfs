a9c0537aad2969ed5972c173e409a885b80130c5
Robert Schmidtke <ro.schmidtke@gmail.com>
2015-11-18 19:05:55 +0100

    contrib: hadoop: added parameter to pass in Hadoop version and added adaptive behavior in listStatus(...) based on Hadoop version

--- contrib/hadoop/src/main/java/org/xtreemfs/common/clients/hadoop/XtreemFSFileSystem.java
+++ contrib/hadoop/src/main/java/org/xtreemfs/common/clients/hadoop/XtreemFSFileSystem.java
@@ -56,6 +56,7 @@ import org.xtreemfs.pbrpc.generatedinterfaces.MRC.Stat;
  */
 public class XtreemFSFileSystem extends FileSystem {
 
+    private int[]               hadoopVersion;
     private URI                 fileSystemURI;
     private Client              xtreemfsClient;
     private Map<String, Volume> xtreemfsVolumes;
@@ -68,6 +69,10 @@ public class XtreemFSFileSystem extends FileSystem {
     private int                 writeBufferSize;
     private Volume              defaultVolume;
     private static final int    STANDARD_DIR_PORT = 32638;
+    private static final int[]  MIN_HADOOP_VERSION = { 0, 0, 0 };
+    private static final int[] MAX_HADOOP_VERSION =
+            { 2, Integer.MAX_VALUE, Integer.MAX_VALUE };
+    private static String STANDARD_HADOOP_VERSION = "2.2.0";
 
     @Override
     public void initialize(URI uri, Configuration conf) throws IOException {
@@ -85,6 +90,36 @@ public class XtreemFSFileSystem extends FileSystem {
         Logging.start(logLevel, Logging.Category.all);
         if (Logging.isDebug()) {
             Logging.logMessage(Logging.LEVEL_DEBUG, this, "init : " + uri);
+        }
+        
+        // Check which Hadoop version this adapter has to support
+        String hadoopVersionString = conf.get("xtreemfs.hadoop.version",
+                STANDARD_HADOOP_VERSION);
+        String[] hadoopVersionSplit = hadoopVersionString.split("\\.");
+        if (hadoopVersionSplit.length < 1 || hadoopVersionSplit.length > 3) {
+            throw new IOException("Unsupported Hadoop version: '"
+                + hadoopVersionString + "'");
+        }
+        hadoopVersion = new int[3];
+        for (int i = 0; i < 3; ++i) {
+            try {
+                hadoopVersion[i] = i < hadoopVersionSplit.length ?
+                        Integer.parseInt(hadoopVersionSplit[i]) : 0;
+            } catch (NumberFormatException e) {
+                throw new IOException("Unsupported Hadoop version: '"
+                        + hadoopVersionString + "'");
+            }
+        }
+        if (compareHadoopVersion(MIN_HADOOP_VERSION) == -1
+                || compareHadoopVersion(MAX_HADOOP_VERSION) == 1) {
+            throw new IOException("Unsupported Hadoop version: "
+                    + hadoopVersion[0] + "." + hadoopVersion[1] + "."
+                    + hadoopVersion[2]);
+        }
+        if (Logging.isDebug()) {
+            Logging.logMessage(Logging.LEVEL_DEBUG, this,
+                    "Running compatible to Hadoop %d.%d.%d",
+                    hadoopVersion[0], hadoopVersion[1], hadoopVersion[2]);
         }
 
         String defaultVolumeName = conf.get("xtreemfs.defaultVolumeName");
@@ -542,7 +577,20 @@ public class XtreemFSFileSystem extends FileSystem {
         }
 
         if (isXtreemFSDirectory(pathString, xtreemfsVolume) == false) {
-            throw new FileNotFoundException(pathString);
+            if (compareHadoopVersion(new int[] { 2, 0, 0 }) >= 0) {
+                // Hadoop 2.x expects a FNE
+                throw new FileNotFoundException(pathString);
+            } else if (compareHadoopVersion(new int[] { 1, 0, 0 }) >= 0) {
+                // Hadoop 1.x expects null
+                return null;
+            } else if (compareHadoopVersion(new int[] { 0, 21, 0 }) >= 0) {
+                // Hadoop 0.21 - 0.23.x expects a FNE
+                throw new FileNotFoundException(pathString);
+            } else {
+                // Hadoop 0.18 - 0.22.x expects null
+                // and Hadoop 0.0 - 0.17.x does not expect anything
+                return null;
+            }
         }
 
         DirectoryEntries dirEntries = xtreemfsVolume.readDir(userCredentials, pathString, 0, 0, false);
@@ -780,5 +828,16 @@ public class XtreemFSFileSystem extends FileSystem {
             return true;
         } else
             return false;
+    }
+    
+    private int compareHadoopVersion(int[] version) {
+        for (int i = 0; i < 3; ++i) {
+            if (hadoopVersion[i] < version[i]) {
+                return -1;
+            } else if (hadoopVersion[i] > version[i]) {
+                return 1;
+            }
+        }
+        return 0;
     }
 }
