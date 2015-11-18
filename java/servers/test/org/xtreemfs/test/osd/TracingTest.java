890f90e92525e4b18a52c5ed537b3f6ec519ee7b
Johannes Dillmann <jdillmann.xtreemfs@gmail.com>
2015-11-18 12:44:27 +0100

    servers: Fix missing IOException export from automerge.

--- java/servers/test/org/xtreemfs/test/osd/TracingTest.java
+++ java/servers/test/org/xtreemfs/test/osd/TracingTest.java
@@ -14,6 +14,7 @@ import static org.junit.Assert.fail;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
 import java.util.concurrent.atomic.AtomicLong;
