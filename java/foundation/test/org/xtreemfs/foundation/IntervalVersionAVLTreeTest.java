aa6e8b112820218d2cc10cb3f318cf9e2536ef74
Johannes Dillmann <jdillmann.xtreemfs@gmail.com>
2016-04-29 19:35:18 +0200

    foundation: IntervalVersionTree: Add more tests and a printTree helper for debugging.

--- java/foundation/test/org/xtreemfs/foundation/IntervalVersionAVLTreeTest.java
+++ java/foundation/test/org/xtreemfs/foundation/IntervalVersionAVLTreeTest.java
@@ -115,6 +115,15 @@ public class IntervalVersionAVLTreeTest {
         tree.insert(0, 2048, 1);
         versions = tree.getVersions(0, 2048);
         assertTrue(expected.containsAll(versions));
+
+        // Test with full tree of height 2, where the first and the last leaf are shrinked
+        tree = createFullTestTree();
+        expected.clear();
+        expected.add(new Interval(0, 500, 0));
+        expected.add(new Interval(500, 4000, 7));
+        expected.add(new Interval(4000, 4096, 6));
+        tree.insert(new Interval(500, 4000, 7));
+        assertEquals(expected, tree.getVersions(0, 4096));
     }
 
     @Test
@@ -194,7 +203,23 @@ public class IntervalVersionAVLTreeTest {
         tree.insert(0, 5, 1);
         tree.insert(500, 511, 1);
         tree.insert(500, 2047, 2);
+        printTree(tree.root);
+        fail();
+    }
 
+    @Test
+    public void testCompact() {
+        IntervalVersionAVLTree tree = new IntervalVersionAVLTree(0, 1);
+        tree.insert(new Interval(1, 2, 0));
+        tree.insert(new Interval(2, 3, 0));
+
+        tree = createFullTestTree();
+        printTree(tree.root);
+
+        System.out.println(tree);
+        tree = tree.compact();
+        System.out.println(tree);
+        fail();
     }
 
     @Test
@@ -213,17 +238,21 @@ public class IntervalVersionAVLTreeTest {
 
     @Test
     public void testTruncate() {
+        LinkedList<Interval> expected = new LinkedList<Interval>();
+
         IntervalVersionAVLTree tree = new IntervalVersionAVLTree(0, 10);
+        expected.add(new Interval(0, 10, 0));
+
         // truncate without effect
         tree.truncate(20);
         assertEquals(10, tree.highest);
-        assertEquals("([0:10], 0)", tree.toString());
+        assertEquals(expected, tree.getVersions(0, 20));
 
         tree.truncate(10);
         assertEquals(10, tree.highest);
-        assertEquals("([0:10], 0)", tree.toString());
+        assertEquals(expected, tree.getVersions(0, 20));
 
-        // truncate to 0 should throw an exception
+        // truncate to less then 0 should throw an exception
         try {
             tree.truncate(-1);
             fail();
@@ -233,16 +262,18 @@ public class IntervalVersionAVLTreeTest {
 
         // simple truncate on a single interval
         tree.truncate(5);
+        expected.clear();
+        expected.add(new Interval(0, 5, 0));
         assertEquals(5, tree.highest);
-        assertEquals("([0:5], 0)", tree.toString());
+        assertEquals(expected, tree.getVersions(0, 20));
 
         // simple truncate on a single interval to the begin
         tree.truncate(1);
+        expected.clear();
+        expected.add(new Interval(0, 1, 0));
         assertEquals(1, tree.highest);
-        assertEquals("([0:1], 0)", tree.toString());
+        assertEquals(expected, tree.getVersions(0, 20));
 
-        LinkedList<Interval> expected = new LinkedList<Interval>();
-        LinkedList<Interval> result;
 
         // test dropping the right tree
         tree = createFullTestTree();
@@ -314,6 +345,119 @@ public class IntervalVersionAVLTreeTest {
         IntervalVersionAVLTree tree = new IntervalVersionAVLTree(0, 4096);
         tree.root = root;
         return tree;
+    }
+
+    public static void printTree(IntervalNode root) {
+        List<List<String>> lines = new ArrayList<List<String>>();
+
+        List<IntervalNode> level = new ArrayList<IntervalNode>();
+        List<IntervalNode> next = new ArrayList<IntervalNode>();
+
+        level.add(root);
+        int nn = 1;
+
+        int widest = 0;
+
+        while (nn != 0) {
+            List<String> line = new ArrayList<String>();
+
+            nn = 0;
+
+            for (IntervalNode n : level) {
+                if (n == null) {
+                    line.add(null);
+
+                    next.add(null);
+                    next.add(null);
+                } else {
+                    String aa = n.toString();
+                    line.add(aa);
+                    if (aa.length() > widest)
+                        widest = aa.length();
+
+                    next.add(n.left);
+                    next.add(n.right);
+
+                    if (n.left != null)
+                        nn++;
+                    if (n.right != null)
+                        nn++;
+                }
+            }
+
+            if (widest % 2 == 1)
+                widest++;
+
+            lines.add(line);
+
+            List<IntervalNode> tmp = level;
+            level = next;
+            next = tmp;
+            next.clear();
+        }
+
+        int perpiece = lines.get(lines.size() - 1).size() * (widest + 4);
+        for (int i = 0; i < lines.size(); i++) {
+            List<String> line = lines.get(i);
+            int hpw = (int) Math.floor(perpiece / 2f) - 1;
+
+            if (i > 0) {
+                for (int j = 0; j < line.size(); j++) {
+
+                    // split node
+                    char c = ' ';
+                    if (j % 2 == 1) {
+                        if (line.get(j - 1) != null) {
+                            c = (line.get(j) != null) ? '┴' : '┘';
+                        } else {
+                            if (j < line.size() && line.get(j) != null)
+                                c = '└';
+                        }
+                    }
+                    System.out.print(c);
+
+                    // lines and spaces
+                    if (line.get(j) == null) {
+                        for (int k = 0; k < perpiece - 1; k++) {
+                            System.out.print(" ");
+                        }
+                    } else {
+
+                        for (int k = 0; k < hpw; k++) {
+                            System.out.print(j % 2 == 0 ? " " : "─");
+                        }
+                        System.out.print(j % 2 == 0 ? "┌" : "┐");
+                        for (int k = 0; k < hpw; k++) {
+                            System.out.print(j % 2 == 0 ? "─" : " ");
+                        }
+                    }
+                }
+                System.out.println();
+            }
+
+            // print line of numbers
+            for (int j = 0; j < line.size(); j++) {
+
+                String f = line.get(j);
+                if (f == null)
+                    f = "";
+                int gap1 = (int) Math.ceil(perpiece / 2f - f.length() / 2f);
+                int gap2 = (int) Math.floor(perpiece / 2f - f.length() / 2f);
+
+                // a number
+                for (int k = 0; k < gap1; k++) {
+                    System.out.print(" ");
+                }
+                System.out.print(f);
+                for (int k = 0; k < gap2; k++) {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+
+            perpiece /= 2;
+        }
+
     }
 
 }
