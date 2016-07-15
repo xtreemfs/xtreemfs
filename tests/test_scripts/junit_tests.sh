84794942d029270d3da0fa1c6dd2a7d78c310d76
Robert Schmidtke <ro.schmidtke@gmail.com>
2016-07-15 16:18:39 +0200

    remove JUnit Maven Debug log level

--- tests/test_scripts/junit_tests.sh
+++ tests/test_scripts/junit_tests.sh
@@ -16,5 +16,5 @@ then
 fi
 echo "TEST_DIR: $TEST_DIR"
 
-mvn --debug --settings $XTREEMFS/java/settings.xml --activate-profiles xtreemfs-dev --file $XTREEMFS/java/pom.xml test 2>&1 | tee "${TEST_DIR}/log/junit.log"
+mvn --settings $XTREEMFS/java/settings.xml --activate-profiles xtreemfs-dev --file $XTREEMFS/java/pom.xml test 2>&1 | tee "${TEST_DIR}/log/junit.log"
 exit ${PIPESTATUS[0]}
